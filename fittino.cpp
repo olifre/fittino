4f5886cdf4492fbb7c07b8fdcd4df5e54af8f026
Peter Wienemann <wienemann@physik.uni-bonn.de>
2005-02-16 17:13:33 +0000

    Version number changed in output file.

--- fittino.cpp
+++ fittino.cpp
@@ -2088,7 +2088,7 @@ void Fittino::writeResults(const char* filename)
 
 
     fprintf(file,"#################### Fittino Fit Summary ####################\n");
-    fprintf(file,"created by Fittino version 1.0.0\n");
+    fprintf(file,"created by Fittino version 1.0.3\n");
     fprintf(file,"on %s, %s %02d, %d at %02d:%02d:%02d\n", weekday.c_str(), month.c_str(),
 	      mday, year, hour, min, sec);
     fprintf(file,"\n");
