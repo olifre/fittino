f64c73653e85c77e4a63654148bb6c16cf827589
Philip Bechtle <bechtle@physik.uni-bonn.de>
2005-09-26 22:00:23 +0000

    *** empty log message ***

--- input.cpp
+++ input.cpp
@@ -37,23 +37,6 @@ Input::Input(const char* inputfile)
   yyISR       = true;
   FillNameMap();
   
-  /* check whether defaults are given */
-  CheckDefault("massZ"  , ID_Z  ,  91.1876 , 0.0021  );
-  CheckDefault("massW"  , ID_W  ,  80.423  , 0.039   );
-  CheckDefault("massCharm", ID_c,   1.2    , 0.2     );
-  CheckDefault("massBottom", ID_b,  4.5    , 0.25    );
-  CheckDefault("massTop", ID_t  , 174.3    , 5.1     );
-  CheckDefault("massTau", ID_tau,   1.77699, 0.00029 );
-  CheckDefault("alphas" , 0     ,   0.1187 , 0.002  );
-// G_F is skipped because its small uncertainty causes numerical problems with
-// covariance matrix inversion.
-//  CheckDefault("G_F"    , 0     ,   1.16639E-5, 0.000001E-5);
-  CheckDefault("alphaem", 0     , 127.934  , 0.027   );
-
-  cout << "mass = " << mass << endl; 
-  //  cout << "before yyparse" << endl;
-  //  DumpMeasuredVector();
-  
   // Open input file and parse it
   yyin = fopen(inputfile, "r");
   if (!yyin) {
@@ -68,6 +51,21 @@ Input::Input(const char* inputfile)
     cerr<<"       Check above messages for details"<<endl;
     exit(EXIT_FAILURE);
   }
+
+  /* check whether defaults are given */
+  CheckDefault("massZ"  , ID_Z  ,  91.1876 , 0.0021  );
+  CheckDefault("massW"  , ID_W  ,  80.423  , 0.039   );
+  CheckDefault("massCharm", ID_c,   1.2    , 0.2     );
+  CheckDefault("massBottom", ID_b,  4.5    , 0.25    );
+  CheckDefault("massTop", ID_t  , 174.3    , 5.1     );
+  CheckDefault("massTau", ID_tau,   1.77699, 0.00029 );
+  CheckDefault("alphas" , 0     ,   0.1187 , 0.002  );
+// G_F is skipped because its small uncertainty causes numerical problems with
+// covariance matrix inversion.
+//  CheckDefault("G_F"    , 0     ,   1.16639E-5, 0.000001E-5);
+  CheckDefault("alphaem", 0     , 127.934  , 0.027   );
+
+  // cout << "mass = " << mass << endl; 
 
   //  cout << "after yyparse" << endl;
   //  DumpMeasuredVector();
