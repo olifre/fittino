7c81b9168f807ea9f2ae488c02ff17edaec2de33
Peter Wienemann <wienemann@physik.uni-bonn.de>
2009-12-22 14:55:13 +0000

    Added Fittino2 code template files

---
+++ devel/FittinoClassTemplate.cpp
@@ -0,0 +1,33 @@
+/* $Id$ */
+
+/*******************************************************************************
+*                                                                              *
+* Project     Fittino - A SUSY Parameter Fitting Package                       *
+*                                                                              *
+* File        FittinoClassTemplate.cpp                                         *
+*                                                                              *
+* Description Fittino class template                                           *
+*                                                                              *
+* Authors     Philip  Bechtle     <philip.bechtle@desy.de>                     *
+*             Klaus   Desch       <desch@physik.uni-bonn.de>                   *
+*	      Mathias Uhlenbrock  <uhlenbrock@physik.uni-bonn.de>              *
+*	      Peter   Wienemann   <wienemann@physik.uni-bonn.de>               *
+*                                                                              *
+* Licence     This program is free software; you can redistribute it and/or    *
+*             modify it under the terms of the GNU General Public License as   *
+*	      published by the Free Software Foundation; either version 3 of   *
+*	      the License, or (at your option) any later version.              *
+*                                                                              *
+*******************************************************************************/
+
+#include "FittinoClassTemplate.h"
+
+FittinoClassTemplate::FittinoClassTemplate() {
+
+    // Code goes here
+
+}
+
+FittinoClassTemplate::~FittinoClassTemplate() {
+
+}
---
+++ devel/FittinoClassTemplate.h
@@ -0,0 +1,38 @@
+/* $Id$ */
+
+/*******************************************************************************
+*                                                                              *
+* Project     Fittino - A SUSY Parameter Fitting Package                       *
+*                                                                              *
+* File        FittinoClassTemplate.h                                           *
+*                                                                              *
+* Description Fittino class template                                           *
+*                                                                              *
+* Authors     Philip  Bechtle     <philip.bechtle@desy.de>                     *
+*             Klaus   Desch       <desch@physik.uni-bonn.de>                   *
+*	      Mathias Uhlenbrock  <uhlenbrock@physik.uni-bonn.de>              *
+*	      Peter   Wienemann   <wienemann@physik.uni-bonn.de>               *
+*                                                                              *
+* Licence     This program is free software; you can redistribute it and/or    *
+*             modify it under the terms of the GNU General Public License as   *
+*	      published by the Free Software Foundation; either version 3 of   *
+*	      the License, or (at your option) any later version.              *
+*                                                                              *
+*******************************************************************************/
+
+/*!
+ *  \brief Fittino class template
+ */
+
+#ifndef FITTINO_FITTINOCLASSTEMPLATE_H
+#define FITTINO_FITTINOCLASSTEMPLATE_H
+
+class FittinoClassTemplate {
+
+  public:
+    FittinoClassTemplate();
+    ~FittinoClassTemplate();
+
+};
+
+#endif // FITTINO_FITTINOCLASSTEMPLATE_H
