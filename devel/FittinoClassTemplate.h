321643b64d05aaab68c2dc5e4a2bc834dc9edd3e
Peter Wienemann <wienemann@physik.uni-bonn.de>
2009-12-23 12:36:22 +0000

    Changed author field

--- devel/FittinoClassTemplate.h
+++ devel/FittinoClassTemplate.h
@@ -8,10 +8,7 @@
 *                                                                              *
 * Description Fittino class template                                           *
 *                                                                              *
-* Authors     Philip  Bechtle     <philip.bechtle@desy.de>                     *
-*             Klaus   Desch       <desch@physik.uni-bonn.de>                   *
-*	      Mathias Uhlenbrock  <uhlenbrock@physik.uni-bonn.de>              *
-*	      Peter   Wienemann   <wienemann@physik.uni-bonn.de>               *
+* Authors     Name Surname <email@domain>                                      *
 *                                                                              *
 * Licence     This program is free software; you can redistribute it and/or    *
 *             modify it under the terms of the GNU General Public License as   *
