0f89deccb58dfc035b24edd0a5ee051e1dca4846
Mathias Uhlenbrock <uhlenbrock@physik.uni-bonn.de>
2010-04-16 12:06:37 +0000

    Corrected typo

--- sources/exceptions/include/LesHouchesFileException.h
+++ sources/exceptions/include/LesHouchesFileException.h
@@ -10,7 +10,7 @@
 *             of problems with Les Houches file (syntax errors, etc.) from     *
 *             model calculators.                                               *
 *                                                                              *
-* Authors     Mahtias Uhlenbrock  <uhlenbrock@physik.uni-bonn.de>              *
+* Authors     Mathias Uhlenbrock  <uhlenbrock@physik.uni-bonn.de>              *
 *             Peter   Wienemann   <wienemann@physik.uni-bonn.de>               *
 *                                                                              *
 * Licence     This program is free software; you can redistribute it and/or    *
