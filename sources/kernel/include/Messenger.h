b4e71e008bed30119e24f6a8df17ad4181fcc60c
Mathias Uhlenbrock <uhlenbrock@physik.uni-bonn.de>
2010-04-16 13:13:21 +0000

    Added class skeleton to handle messages

---
+++ sources/kernel/include/Messenger.h
@@ -0,0 +1,47 @@
+/* $Id$ */
+
+/*******************************************************************************
+*                                                                              *
+* Project     Fittino - A SUSY Parameter Fitting Package                       *
+*                                                                              *
+* File        Messenger.h                                                      *
+*                                                                              *
+* Description Class for messages                                               *
+*                                                                              *
+* Authors     Mathias Uhlenbrock  <uhlenbrock@physik.uni-bonn.de>              *
+*                                                                              *
+* Licence     This program is free software; you can redistribute it and/or    *
+*             modify it under the terms of the GNU General Public License as   *
+*	      published by the Free Software Foundation; either version 3 of   *
+*	      the License, or (at your option) any later version.              *
+*                                                                              *
+*******************************************************************************/
+
+#ifndef FITTINO_MESSENGER_H
+#define FITTINO_MESSENGER_H
+
+/*!
+ *  \brief Fittino namespace
+ */
+namespace Fittino {
+
+  /*!
+   *  \brief Class for messages 
+   */
+  class Messenger {
+
+    public:
+      /*!
+       *  Constructor
+       */
+      Messenger();
+      /*!
+       *  Destructor
+       */
+      ~Messenger();
+
+  };
+
+}
+
+#endif // FITTINO_MESSENGER_H
---
+++ sources/kernel/src/Messenger.cpp
@@ -0,0 +1,28 @@
+/* $Id$ */
+
+/*******************************************************************************
+*                                                                              *
+* Project     Fittino - A SUSY Parameter Fitting Package                       *
+*                                                                              *
+* File        Messenger.cpp                                                    *
+*                                                                              *
+* Description Class for messages                                               *
+*                                                                              *
+* Authors     Mathias Uhlenbrock  <uhlenbrock@physik.uni-bonn.de>              *
+*                                                                              *
+* Licence     This program is free software; you can redistribute it and/or    *
+*             modify it under the terms of the GNU General Public License as   *
+*	      published by the Free Software Foundation; either version 3 of   *
+*	      the License, or (at your option) any later version.              *
+*                                                                              *
+*******************************************************************************/
+
+#include "Messenger.h"
+
+Fittino::Messenger::Messenger() {
+
+}
+
+Fittino::Messenger::~Messenger() {
+
+}
