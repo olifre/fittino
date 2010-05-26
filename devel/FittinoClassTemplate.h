f08e2b62a80bf829f8b7878e49de22449e7b975e
Mathias Uhlenbrock <uhlenbrock@physik.uni-bonn.de>
2010-05-26 09:42:00 +0000

    Tiny modification of the template headers

--- devel/FittinoClassTemplate.h
+++ devel/FittinoClassTemplate.h
@@ -8,7 +8,7 @@
 *                                                                              *
 * Description Fittino class template                                           *
 *                                                                              *
-* Authors     Name Surname <email@domain>                                      *
+* Authors     Name Surname  <email@domain>                                     *
 *                                                                              *
 * Licence     This program is free software; you can redistribute it and/or    *
 *             modify it under the terms of the GNU General Public License as   *
