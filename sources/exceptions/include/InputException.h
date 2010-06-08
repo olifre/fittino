7052a1beb4776dd0851837e3bccb9fa51dbe7feb
Mathias Uhlenbrock <uhlenbrock@physik.uni-bonn.de>
2010-06-08 08:12:52 +0000

    Changed class name of InputFileException to the more general InputException

---
+++ sources/exceptions/include/InputException.h
@@ -0,0 +1,50 @@
+/* $Id: InputException.h 582 2010-04-15 23:00:32Z uhlenbrock $ */
+
+/*******************************************************************************
+*                                                                              *
+* Project     Fittino - A SUSY Parameter Fitting Package                       *
+*                                                                              *
+* File        InputException.h                                                 *
+*                                                                              *
+* Description Fittino input exception class. It is thrown in case of erroneous *
+*             input.                                                           *
+*                                                                              *
+* Authors     Mathias Uhlenbrock  <uhlenbrock@physik.uni-bonn.de>              *
+*             Peter   Wienemann   <wienemann@physik.uni-bonn.de>               *
+*                                                                              *
+* Licence     This program is free software; you can redistribute it and/or    *
+*             modify it under the terms of the GNU General Public License as   *
+*             published by the Free Software Foundation; either version 3 of   *
+*             the License, or (at your option) any later version.              *
+*                                                                              *
+*******************************************************************************/
+
+#ifndef FITTINO_INPUTEXCEPTION_H
+#define FITTINO_INPUTEXCEPTION_H
+
+#include "ExceptionBase.h"
+
+/*!
+ *  \brief Fittino namespace
+ */
+namespace Fittino {
+
+  /*!
+   *  \brief Fittino input exception class
+   */
+  class InputException : public ExceptionBase {
+
+    public:
+      /*!
+       *  Takes as input a message string which is printed on the screen as soon
+       *  as the exception is caught and the what() method is called. The message
+       *  is supposed to provide the user with further information about the
+       *  occurred exception.
+       */
+      InputException( const std::string& message );
+
+  };
+
+}
+
+#endif // FITTINO_INPUTEXCEPTION_H
---
+++ sources/exceptions/src/InputException.cpp
@@ -0,0 +1,26 @@
+/* $Id: InputException.cpp 582 2010-04-15 23:00:32Z uhlenbrock $ */
+
+/*******************************************************************************
+*                                                                              *
+* Project     Fittino - A SUSY Parameter Fitting Package                       *
+*                                                                              *
+* File        InputException.cpp                                               *
+*                                                                              *
+* Description Fittino input exception class. It is thrown in case of erroneous *
+*             input.                                                           *
+*                                                                              *
+* Authors     Mathias Uhlenbrock  <uhlenbrock@physik.uni-bonn.de>              *
+*                                                                              *
+* Licence     This program is free software; you can redistribute it and/or    *
+*             modify it under the terms of the GNU General Public License as   *
+*	      published by the Free Software Foundation; either version 3 of   *
+*	      the License, or (at your option) any later version.              *
+*                                                                              *
+*******************************************************************************/
+
+#include "InputException.h"
+
+Fittino::InputException::InputException( const std::string& message )
+        : ExceptionBase( "Fittino::InputException: " + message ) {
+
+}
