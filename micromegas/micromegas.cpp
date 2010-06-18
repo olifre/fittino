06f5aa335bd64490646410065449dd107d49e32d
Peter Wienemann <wienemann@physik.uni-bonn.de>
2010-06-18 13:54:18 +0000

    Micromegas code now has all needed functionality

--- micromegas/micromegas.cpp
+++ micromegas/micromegas.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
+#include <string>
 
 #include "micromegas.h"
 #include "pmodel.h"
 
 int main(int argc, char** argv)
 {
-  
-
-  int readSMPar = 1;
-  int err = readLesH("SPheno-sps1aprime.spc", readSMPar);
-  if (err < 0) {
-    std::cerr << "Micromegas failed to open Les Houches file" << std::endl;
-    return err;
+  if (argc != 2) {
+    std::cerr << "usage: micromegas <Les Houches file>" << std::endl;
+    return 1;
   }
 
-  std::cout << "M1 = " << findValW("MG1") << std::endl;
+  char* slhafilename = new char[strlen(argv[1])];
+  strcpy(slhafilename, argv[1]);
+
+  std::cout << "micromegas: reading Les Houches file " << slhafilename << std::endl;
+
+  int readSMPar = 1;
+  int err = readLesH(slhafilename, readSMPar);
+  if (err < 0) {
+    std::cerr << "micromegas failed to open Les Houches file" << std::endl;
+    return err;
+  }
 
   char lspName[128];
   err = sortOddParticles(lspName);
   if (err) {
-    std::cerr << "Micromegas cannot calculate relic density for " << lspName << " LSPs" << std::endl;
+    std::cerr << "micromegas cannot calculate relic density for " << lspName << " LSPs" << std::endl;
     return err;
   }
 
@@ -29,10 +36,28 @@ int main(int argc, char** argv)
   double omegah2 = darkOmega(&Xf, fast, Beps);
   if (omegah2 < 0) {
     std::cerr << "Problem with DM relic density calculation" << std::endl;
-    return omegah2;
+    return 1;
   }
 
-  std::cout << "omega h2 = " << omegah2 << std::endl;
+  std::cout << "omega h^2 = " << omegah2 << std::endl;
+
+  std::string newfile = slhafilename;
+  newfile += ".micromegas";
+
+  char command[1024];
+  sprintf(command, "cp %s %s\n", slhafilename, newfile.c_str());
+  system(command);
+
+  sprintf(command, "echo 'Block Micromegas # Micromegas observables' >> %s\n",
+	  newfile.c_str());
+  system(command);
+
+  sprintf(command, "echo '    1    %.8E   # Omega h^2' >> %s\n", omegah2,
+	  newfile.c_str());
+  system(command);
+
+  sprintf(command, "mv %s %s\n", newfile.c_str(), slhafilename);
+  system(command);
 
   return 0;
 }
