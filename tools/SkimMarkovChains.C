7ba717da7546a9d4fa879bb2dde8e86f0eed84bf
Peter Wienemann <wienemann@physik.uni-bonn.de>
2010-10-11 16:55:00 +0000

    Improved output and more (commented out) Mastercode corrections

--- tools/SkimMarkovChains.C
+++ tools/SkimMarkovChains.C
@@ -28,9 +28,10 @@ void SkimMarkovChains ( string outputRootFileName = "MarkovChainNtupFileSkimmed.
 			double absMinChi2 = 0. ) {
 
   TChain markovChain("markovChain");
-  markovChain.Add("MarkovChainNtupFile*.root");
+  //  markovChain.Add("MarkovChainNtupFile.*.root"); // multiple file chain can give weird results
+  markovChain.Add("MarkovChainNtupFile.sum.root");
   // markovChain.Add("MarkovTestRosenbrockNTupel.root");
-  markovChain.Print();
+  //  markovChain.Print();
 
   int nEntries = markovChain.GetEntries();
   if ( maxevents >= 0 ) {
@@ -39,12 +40,19 @@ void SkimMarkovChains ( string outputRootFileName = "MarkovChainNtupFileSkimmed.
   std::cout << "number of entries " << nEntries << std::endl;
 
   const int nLeaves = markovChain.GetListOfLeaves()->GetEntries();
+
+  if (nLeaves > 10000) {
+    cerr << "Increase leaf array size" << endl;
+    return;
+  }
+
   std::cout << "number of leaves " << nLeaves << std::endl;
 
   float* par   = new float[nLeaves];
   //float* par   = new float[10000];
   //float par[10000];
-  TLeafD* leaf[nLeaves];
+  //  TLeafD* leaf[nLeaves];
+  TLeafD* leaf[10000];
   int iChi2 = -1;
   int iLikelihood = -1;
   int iTanBeta = -1;
@@ -105,31 +113,93 @@ void SkimMarkovChains ( string outputRootFileName = "MarkovChainNtupFileSkimmed.
   for (Int_t iLeaf=0; iLeaf<nLeaves; iLeaf++) {
      string str = leaf[iLeaf]->GetName();
      str.append("/F");
-     //cout << "Adding branch " << yyFittedPar[k].name.c_str() << " to tree" << endl;
+     //     cout << "Adding branch " << leaf[iLeaf]->GetName() << " to tree" << endl;
      newTree->Branch(leaf[iLeaf]->GetName(), &par[iLeaf], str.c_str());
      str.erase();
   }
 
   cout << "looping over events to find minimal chi2 at iChi2 = " << iChi2 << " iLikelihood = " << iLikelihood << endl;
 
+
   // clean up NPFitter bug
-  // absMinChi2 = 31.;
+  //  absMinChi2 = 19.349; // for xsec_realsys
+
+  /*
+  absMinChi2 = 19.31; // for noxsec
+  const int nm0 = 15;
+  double m0[] = { 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 
+                  110, 130, 150, 200};
+  double c2m0[] = {21.78, 20.48, 19.76, 19.46, 19.34, 19.31, 19.31, 19.31, 19.32, 19.35, 19.38,
+                   19.55, 19.75, 19.98, 20.51};
+  TGraph* gm0 = new TGraph(nm0, m0, c2m0);
+
+  const int nm12 = 19;
+  double m12[] = { 220, 230, 240, 250, 260, 280, 300, 320, 340, 360, 380, 400,
+                   450, 500, 600, 700, 800, 900, 1000};
+  double c2m12[] = { 21.72, 20.91, 20.41, 20.07, 19.8, 19.52, 19.37, 19.31, 19.33, 19.47, 19.68, 19.99,
+		     20.9, 21.78, 23.46, 25, 26.5, 27.36, 27.36};
+  TGraph* gm12 = new TGraph(nm12, m12, c2m12);
+
+  const int ntb = 21;
+  double tb[] = {3.04, 3.5, 4, 4.5, 5, 5.5, 6, 7, 8, 9,
+                 10, 11, 12, 14, 16, 18, 20, 25, 30, 35, 40};
+  double c2tb[] = {26, 23.66, 22.11, 21.25, 20.71, 20.36, 20.13, 19.78, 19.59, 19.43,
+                   19.35, 19.32, 19.30, 19.36, 19.48, 19.7, 19.99, 20.83, 22.09, 23.46, 24.16};
+  TGraph* gtb = new TGraph(ntb, tb, c2tb);
+
+  const int na0 = 22;
+  double a0[] = {-2500, -1580, -1300, -1100, -800, -500, -300, -200, -100, 0,
+                 100, 200, 300, 400, 600, 800, 1000, 1200, 1400,
+                 1600, 1800, 2100};
+  double c2a0[] = {26, 26, 24.97, 24.11, 22.67, 21.05, 20.15, 19.84, 19.61, 19.48,
+                   19.39, 19.32, 19.30, 19.30, 19.41, 19.63, 20.06, 20.56, 21.23,
+                   22.76, 23.93, 26};
+  TGraph* ga0 = new TGraph(na0, a0, c2a0);
+  */
 
   // find minimal chi2
-  float minChi2 = 10000000.;
+  float minChi2 = 1e20;
+  float minM0 = -1;
+  float minM12 = -1;
+  float minTanBeta = -1;
+  float minA0 = 0;
   for (Int_t i=0; i<nEntries; i++) {
     markovChain.GetEntry(i);	
     float chi2 =  par[iChi2];
     if (chi2<0.) {
       continue;
     }   
-    // cout << "chi2 = " << chi2 << " " << par[iChi2] <<  " " << par[0] << endl; 
+    //    cout << "chi2 = " << chi2 << endl; 
     if (chi2<minChi2 && chi2>=absMinChi2) {
+      /*
+      if ( par[iM0] > m0[0] && par[iM0] < m0[nm0-1]) {
+	if (chi2 < gm0->Eval(par[iM0])) continue;
+      }
+      if ( par[iM12] > m12[0] && par[iM12] < m12[nm12-1]) {
+	if (chi2 < gm12->Eval(par[iM12])) continue;
+      }
+      if ( par[iTanBeta] > tb[0] && par[iTanBeta] < tb[ntb-1]) {
+	if (chi2 < gtb->Eval(par[iTanBeta])) continue;
+      }
+      if ( par[iA0] > a0[0] && par[iA0] < a0[na0-1]) {
+	if (chi2 < ga0->Eval(par[iA0])) continue;
+      }
+      */
+      if (iM0 >= 0) minM0 = par[iM0];
+      if (iM12 >= 0) minM12 = par[iM12];
+      if (iA0 >= 0) minA0 = par[iA0];
+      if (iTanBeta >= 0) minTanBeta = par[iTanBeta];
       minChi2 = chi2;
+      cout << "found new chi2 minimum with chi2 = " << minChi2 << " at" << endl; 
+      cout << "M0 = " << minM0 << "  M12 = " << minM12 
+	   << "  TanBeta = " << minTanBeta << "   A0 = " << minA0 << endl;
     }
   }
 
   cout << "minimal chi2 found at " << minChi2 << " for iChi2 = " << iChi2 << endl;
+  cout << "corresponding parameter point:" << endl;
+  cout << "M0 = " << minM0 << "  M12 = " << minM12 
+       << "  TanBeta = " << minTanBeta << "   A0 = " << minA0 << endl;
 
   // fill events
   int nNewEvents = 0;
@@ -193,6 +263,20 @@ void SkimMarkovChains ( string outputRootFileName = "MarkovChainNtupFileSkimmed.
 //	}
 //      }
 //    }
+/*
+    if ( par[iM0] > m0[0] && par[iM0] < m0[nm0-1]) {
+      if (chi2 < gm0->Eval(par[iM0])) continue;
+    }
+    if ( par[iM12] > m12[0] && par[iM12] < m12[nm12-1]) {
+      if (chi2 < gm12->Eval(par[iM12])) continue;
+    }
+    if ( par[iTanBeta] > tb[0] && par[iTanBeta] < tb[ntb-1]) {
+      if (chi2 < gtb->Eval(par[iTanBeta])) continue;
+    }
+    if ( par[iA0] > a0[0] && par[iA0] < a0[na0-1]) {
+      if (chi2 < ga0->Eval(par[iA0])) continue;
+    }
+*/
     // end clean up
     if (iTanBeta>=0) {
       if (par[iTanBeta]>100. || par[iTanBeta]<0.) {
@@ -241,3 +325,4 @@ void SkimMarkovChains ( string outputRootFileName = "MarkovChainNtupFileSkimmed.
   return;
 
 }
+
