7d6745d0dc2248be15931482570f310c1b2e4075
Björn Sarrazin <sarrazin@physik.uni-bonn.de>
2011-01-16 22:47:42 +0000

    made compilable and added function which determines a meaningful name for the cut itself

--- tools/CreateCutObject.C
+++ tools/CreateCutObject.C
@@ -1,7 +1,7 @@
 
 //This very simple macro hopefully helps to create TCutG objects with the graphics editor. 
 //Start root and load the root file with the NTuple you want to skim, e.g.: root -l MarkovChainNtupfile.root
-//Load this file: .L CreateCutObject.C
+//Load this file: .L CreateCutObject.C++
 //Draw the variables you want use for skimming, e.g. markovChain->Draw("chi2:P_M0", "chi2<35")
 //In the canvas, click on View->ToolBar. Select the graphical cut tool on the right hand side.
 //One short mouse click determines the first point of the polygon.
@@ -11,20 +11,84 @@
 //Go on with the next variable: markovChain->Draw("chi2:P_M12", "chi2<35")...
 
 
-void CreateCutObject(string cutfile, string cutname){
+#include "TKey.h" 
+#include "TFile.h" 
+#include "TCutG.h" 
+#include "TDirectory.h" 
+#include "TList.h" 
+#include "TROOT.h" 
+#include "iostream" 
+
+
+void CreateCutObject(const char* cutfile, const char* cutname){
+
+  TFile* file=(TFile*) gROOT->GetListOfFiles()->At(0);
 
   TCutG *mycutg;                                                                                                                                                                                  
 
   mycutg = (TCutG*)gROOT->GetListOfSpecials()->FindObject("CUTG");
 
-  mycutg->SetName(cutname.c_str());
-
-  TFile f(cutfile.c_str(), "update");
+  mycutg->SetName(cutname);
+  
+  TFile Cutfile(cutfile, "update");
+  Cutfile.cd();
 
   mycutg->Write(); 
 
-  f.Close();
+  Cutfile.Close();
 
-  _file0->cd();
+  file->cd();
+
+}
+
+
+
+void CreateCutObject(const char* cutfile){
+
+  TFile* file=(TFile*) gROOT->GetListOfFiles()->At(0);
+
+  TCutG *mycutg;                                                                                                                                                                                  
+
+  mycutg = (TCutG*)gROOT->GetListOfSpecials()->FindObject("CUTG");
+
+  const char* varx=mycutg->GetVarX();
+  
+  int NumberOfCutsOfThatVariable=0;
+
+  TFile Cutfile(cutfile, "update");
+  
+  TList *listOfKeys = (TList*) Cutfile.GetListOfKeys();
+
+  TIter next(listOfKeys);
+  
+  while (TKey* key = (TKey*) next()) {
+
+    TObject *obj = key->ReadObj();
+
+    const char* name=obj->GetName();
+    
+    if (!strncmp(varx,name,strlen(varx))){
+
+      NumberOfCutsOfThatVariable++;
+
+    }
+
+  }
+  
+  char CutsOfThatVariable[10];
+  sprintf(CutsOfThatVariable,"%d", NumberOfCutsOfThatVariable);
+
+  char newname[200];
+  strcpy(newname,varx);
+  strcat(newname, "_");
+  strcat(newname, CutsOfThatVariable); 
+  cout<<"Save cut as "<<newname<<endl;
+  mycutg->SetName(newname);
+  
+  Cutfile.cd();
+  mycutg->Write(); 
+  Cutfile.Close();
+  
+  file->cd();
 
 }
