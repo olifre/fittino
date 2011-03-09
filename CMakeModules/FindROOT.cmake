6c15bd69da8fa4643ed588a77be79ee1dc13399d
Peter Wienemann <wienemann@physik.uni-bonn.de>
2011-03-09 15:33:33 +0000

    Added ROOT version check

--- CMakeModules/FindROOT.cmake
+++ CMakeModules/FindROOT.cmake
@@ -21,7 +21,8 @@
 # Specify the installation path of the executable "root-config". By default, it is assumed that the
 # environment variable ROOTSYS is set.
 
-# Requires at least ROOT version 5.20.00
+# Requires at least ROOT version 5.20/00
+set(ROOT_MIN_VERSION "5.20/00")
 
 SET(ROOT_CONFIG_PATH $ENV{ROOTSYS}/bin)
 
@@ -36,15 +37,38 @@ FIND_PROGRAM(ROOT_CONFIG_EXECUTABLE root-config PATHS ${ROOT_CONFIG_PATH} NO_DEF
 
 IF(${ROOT_CONFIG_EXECUTABLE} MATCHES "ROOT_CONFIG_EXECUTABLE-NOTFOUND")
 
-    # If the executable "root-config is not found print this message"
+    # If the executable "root-config" is not found print this message
 
     MESSAGE(FATAL_ERROR "\nModule ROOT not found.\nPlease set ROOTSYS or add the path to your ROOT installation to the macro FindROOT.cmake in the subdirectory CMakeModules.\n") 
 
 ELSE(${ROOT_CONFIG_EXECUTABLE} MATCHES "ROOT_CONFIG_EXECUTABLE-NOTFOUND")
 
-    # If the executable "root-config is found print this message"
+    # If the executable "root-config" is found, check version number
+    EXECUTE_PROCESS(COMMAND root-config --version OUTPUT_VARIABLE ROOT_FOUND_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-    MESSAGE(STATUS "Module ROOT found")
+    # Decompose minimal ROOT version into major, minor and patch numbers
+    string(REGEX REPLACE "^([0-9]+)\\.[0-9][0-9]+\\/[0-9][0-9]+" "\\1" ROOT_MIN_VERSION_MAJOR "${ROOT_MIN_VERSION}")
+    string(REGEX REPLACE "^[0-9]+\\.([0-9][0-9])+\\/[0-9][0-9]+.*" "\\1" ROOT_MIN_VERSION_MINOR "${ROOT_MIN_VERSION}")
+    string(REGEX REPLACE "^[0-9]+\\.[0-9][0-9]+\\/([0-9][0-9]+)" "\\1" ROOT_MIN_VERSION_PATCH "${ROOT_MIN_VERSION}")
+
+    # Decompose found ROOT version into major, minor and patch numbers
+    string(REGEX REPLACE "^([0-9]+)\\.[0-9][0-9]+\\/[0-9][0-9]+" "\\1" ROOT_FOUND_VERSION_MAJOR "${ROOT_FOUND_VERSION}")
+    string(REGEX REPLACE "^[0-9]+\\.([0-9][0-9])+\\/[0-9][0-9]+.*" "\\1" ROOT_FOUND_VERSION_MINOR "${ROOT_FOUND_VERSION}")
+    string(REGEX REPLACE "^[0-9]+\\.[0-9][0-9]+\\/([0-9][0-9]+)" "\\1" ROOT_FOUND_VERSION_PATCH "${ROOT_FOUND_VERSION}")
+
+    # Translate version numbers into single (easily comparable) numbers
+    MATH(EXPR MIN_VERSION "${ROOT_MIN_VERSION_MAJOR}*10000 + ${ROOT_MIN_VERSION_MINOR}*100 + ${ROOT_MIN_VERSION_PATCH}")
+    MATH(EXPR FOUND_VERSION "${ROOT_FOUND_VERSION_MAJOR}*10000 + ${ROOT_FOUND_VERSION_MINOR}*100 + ${ROOT_FOUND_VERSION_PATCH}")
+
+    IF(FOUND_VERSION LESS MIN_VERSION)
+
+      MESSAGE(FATAL_ERROR "\nROOT version too old. At least ${ROOT_MIN_VERSION} needed.\n")
+
+    ELSE(FOUND_VERSION LESS MIN_VERSION)
+
+      MESSAGE(STATUS "ROOT version: ${ROOT_FOUND_VERSION}")
+
+    ENDIF(FOUND_VERSION LESS MIN_VERSION)
 
     # Look for needed libraries and make them accessible via the ROOT_LIBRARIES cache entry.
 
@@ -57,167 +81,3 @@ ELSE(${ROOT_CONFIG_EXECUTABLE} MATCHES "ROOT_CONFIG_EXECUTABLE-NOTFOUND")
     SET(ROOT_LIBRARIES ${ROOT_STANDARD_LIBRARIES} ${ROOT_XMLPARSER_LIBRARY} ${ROOT_MINUIT2_LIBRARY})
 
 ENDIF(${ROOT_CONFIG_EXECUTABLE} MATCHES "ROOT_CONFIG_EXECUTABLE-NOTFOUND")
-
-# Folgendes funktioniert leider nicht, da der Output in einem String gespeichert ist
-# und nicht in einer Liste. Dies muss ich mir noch mal anschauen.
-# Bis dahin wird jede Library einzeln abgefragt und am Ende die ROOT_LIBRARIES
-# zusammengebaut.
-#
-#      EXECUTE_PROCESS( ${ROOT_CONFIG_EXECUTABLE}
-#        ARGS "--libs --noauxlibs --noldflags" 
-#        OUTPUT_VARIABLE root_flags )
-#        message("ROOT_FLAGS: ${root_flags}")
-#
-#     set(ROOT_LIBRARIES "")
-#     FOREACH (_current_FLAG ${root_flags})
-#       message("1")
-#       string(SUBstring ${_current_FLAG} 2 1 bla)
-#       if(${bla} MATCHES "l")
-#         string(REGEX REPLACE "^-l(.*)" "\\1" test ${_current_FLAG}) 
-#         message("${test}")
-#         set(ROOT_LIBRARIES ${ROOT_LIBRARIES} ${test})
-#       ELSE (${bla} MATCHES "l")
-#         if(${bla} MATCHES "p")
-#           string(REGEX REPLACE "^-(.*)" "\\1" test ${_current_FLAG}) 
-#         endif(${bla} MATCHES "p")       
-#         message("${test}")
-#         set(ROOT_LIBRARIES ${ROOT_LIBRARIES} ${test})
-#       ENDIF (${bla} MATCHES "l")
-#
-#     endFOREACH (_current_FLAG ${root_flags})
-#
-#
-#      set(ROOT_FLAGS ${root_flags})
-#      # CACHE INTERNAL "")
-#
-#
-#  set(ROOT_DEFINITIONS ${ROOT_DEFINITIONS} -L${ROOT_LIBRARY_DIR} ${ROOT_FLAGS} )
-
-# Make variables changeble to the advanced user
-#MARK_AS_ADVANCED( ROOT_LIBRARY_DIR ROOT_INCLUDE_DIR ROOT_DEFINITIONS)
-
-# Set ROOT_INCLUDES
-#set( ROOT_INCLUDES ${ROOT_INCLUDE_DIR})
-
-#set(LD_LIBRARY_PATH ${LD_LIBRARY_PATH} ${ROOT_LIBRARY_DIR})
- 
-
-#######################################
-#
-#       Check the executables of ROOT 
-#          ( rootcint )
-#
-#######################################
-
-#    find_program(ROOT_CINT_EXECUTABLE
-#	    NAMES rootcint
-#	    PATHS ${ROOT_BINARY_DIR}
-#	    NO_DEFAULT_PATH
-#	    )
-
-# Check if the minimal ROOT version required is found
-
-#STRING(REGEX REPLACE "(^.*)/bin/root-config" "\\1" test ${ROOT_CONFIG_EXECUTABLE})
-#SET(ENV{ROOTSYS} ${test})
-
-#if(ROOT_CONFIG_EXECUTABLE)
-#
-#    # we need at least version 4.00/08
-#
-#    if(NOT ROOT_MIN_VERSION)
-#
-#        set(ROOT_MIN_VERSION "4.00/08")
-#
-#    endif(NOT ROOT_MIN_VERSION)
-# 
-#    # now parse the parts of the user given version string into variables
-#
-#    string(REGEX REPLACE "^([0-9]+)\\.[0-9][0-9]+\\/[0-9][0-9]+" "\\1" req_root_major_vers "${ROOT_MIN_VERSION}")
-#    string(REGEX REPLACE "^[0-9]+\\.([0-9][0-9])+\\/[0-9][0-9]+.*" "\\1" req_root_minor_vers "${ROOT_MIN_VERSION}")
-#    string(REGEX REPLACE "^[0-9]+\\.[0-9][0-9]+\\/([0-9][0-9]+)" "\\1" req_root_patch_vers "${ROOT_MIN_VERSION}")
-#    
-#    # and now the version string given by qmake
-#
-#    string(REGEX REPLACE "^([0-9]+)\\.[0-9][0-9]+\\/[0-9][0-9]+.*" "\\1" found_root_major_vers "${ROOTVERSION}")
-#    string(REGEX REPLACE "^[0-9]+\\.([0-9][0-9])+\\/[0-9][0-9]+.*" "\\1" found_root_minor_vers "${ROOTVERSION}")
-#    string(REGEX REPLACE "^[0-9]+\\.[0-9][0-9]+\\/([0-9][0-9]+).*" "\\1" found_root_patch_vers "${ROOTVERSION}")
-#    
-#    if(found_root_major_vers LESS 4)
-#
-#        message(FATAL_ERROR "Invalid ROOT version \"${ROOTERSION}\", at least major version 4 is required, e.g. \"4.00/08\"")
-#
-#    endif(found_root_major_vers LESS 4)
-#    
-#    # compute an overall version number which can be compared at once
-#
-#    math(EXPR req_vers "${req_root_major_vers}*10000 + ${req_root_minor_vers}*100 + ${req_root_patch_vers}")
-#    math(EXPR found_vers "${found_root_major_vers}*10000 + ${found_root_minor_vers}*100 + ${found_root_patch_vers}")
-#    
-#    if(found_vers LESS req_vers)
-#
-#        set(ROOT_FOUND false)
-#        set(ROOT_INSTALLED_VERSION_TOO_OLD true)
-#
-#    else(found_vers LESS req_vers)
-#
-#        set(ROOT_FOUND true)
-#        set(ROOT_INSTALLED_VERSION_TOO_OLD false)
-#
-#    endif(found_vers LESS req_vers)
-#
-#endif(ROOT_CONFIG_EXECUTABLE)
-
-#set(ROOT_DEFINITIONS "")
-
-###########################################
-#
-#       Macros for building ROOT dictionary
-#
-###########################################
-
-#MACRO(ROOT_GENERATE_DICTIONARY)
-#
-#    set(INFILES "")
-#
-#    FOREACH(_current_FILE ${ARGN})
-#    
-#        IF(${_current_FILE} MATCHES "^.*\\.h$")
-#
-#            IF(${_current_FILE} MATCHES "^.*Link.*$")
-#
-#                set(LINKDEF_FILE ${_current_FILE})
-#
-#            ELSE(${_current_FILE} MATCHES "^.*Link.*$")
-#
-#                set(INFILES ${INFILES} ${_current_FILE})
-#
-#            ENDIF(${_current_FILE} MATCHES "^.*Link.*$")
-#
-#        ELSE(${_current_FILE} MATCHES "^.*\\.h$")
-#
-#            IF(${_current_FILE} MATCHES "^.*\\.cxx$")
-#
-#                set(OUTFILE ${_current_FILE})
-#
-#            ELSE(${_current_FILE} MATCHES "^.*\\.cxx$")
-#
-#                set(INCLUDE_DIRS ${INCLUDE_DIRS} -I${_current_FILE})   
-#
-#            ENDIF(${_current_FILE} MATCHES "^.*\\.cxx$")
-#
-#        ENDIF(${_current_FILE} MATCHES "^.*\\.h$")
-#    
-#    ENDFOREACH(_current_FILE ${ARGN})
-#    
-#    #message("INFILES: ${INFILES}")
-#    #message("OutFILE: ${OUTFILE}")
-#    #message("LINKDEF_FILE: ${LINKDEF_FILE}")
-#    #message("INCLUDE_DIRS: ${INCLUDE_DIRS}")
-#    string(REGEX REPLACE "(^.*).cxx" "\\1.h" bla "${OUTFILE}")
-#    #message("BLA: ${bla}")
-#    SET(OUTFILES ${OUTFILE} ${bla})
-#    ADD_CUSTOM_COMMAND(OUTPUT ${OUTFILES}
-#    COMMAND ${ROOT_CINT_EXECUTABLE} ARGS -f ${OUTFILE} -c -DHAVE_CONFIG_H ${INCLUDE_DIRS} ${INFILES} ${LINKDEF_FILE} )
-#    #message("ROOT_CINT_EXECUTABLE has created the dictionary ${OUTFILE}")
-#
-#ENDMACRO(ROOT_GENERATE_DICTIONARY)
