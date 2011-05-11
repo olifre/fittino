0ecf20073483ed9570cce0ffc95db7d8c071a32b
Peter Wienemann <wienemann@physik.uni-bonn.de>
2011-05-11 09:25:09 +0000

    Finished cleaning script

--- afterburner/removeSPhenoBlocks.sh
+++ afterburner/removeSPhenoBlocks.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
+# This script removes non-standard SLHA blocks written by SPheno.
+# This is needed to feed SPheno spectrum files into programs which
+# do not ignore unknown blocks and thus do not work properly.
+
 FILE=SPheno.spc.last
+TMPFILESUFFIX=tmp
 
-START=`cat -n $FILE | grep "Block HiggsBoundsInputHiggsCouplingsBosons" | awk '{ print $1}'`
-NEXT=`cat -n $FILE | grep "Block SPhenoLowEnergy" | awk '{ print $1}'`
-END=`expr $NEXT - 1`
+STARTINFO=`cat -n $FILE | grep "Block SPhenoINFO" | awk '{ print $1}'`
+AFTERINFO=`cat -n $FILE | grep "Block MODSEL" | awk '{ print $1}'`
+ENDINFO=`expr $AFTERINFO - 1`
 
-sed "$START,$END d" $FILE
+sed "$STARTINFO,$ENDINFO d" $FILE > $FILE.${TMPFILESUFFIX}1
 
-echo $START $END
+STARTHB=`cat -n $FILE.${TMPFILESUFFIX}1 | grep "Block HiggsBoundsInputHiggsCouplingsFermions" | awk '{ print $1}'`
+AFTERHB=`cat -n $FILE.${TMPFILESUFFIX}1 | grep "Block SPhenoLowEnergy" | awk '{ print $1}'`
+ENDHB=`expr $AFTERHB - 1`
+
+sed "$STARTHB,$ENDHB d" $FILE.${TMPFILESUFFIX}1 > $FILE.${TMPFILESUFFIX}2
+
+STARTLE=`cat -n $FILE.${TMPFILESUFFIX}2 | grep "Block SPhenoLowEnergy" | awk '{ print $1}'`
+#AFTERLE=`cat -n $FILE.${TMPFILESUFFIX}2 | grep "BLOCK PREDICT" | awk '{ print $1}'`
+#ENDLE=`expr $AFTERLE - 1`
+ENDLE=`wc -l $FILE.${TMPFILESUFFIX}2 | awk '{ print $1 }'`
+
+sed "$STARTLE,$ENDLE d" $FILE.${TMPFILESUFFIX}2 > $FILE.${TMPFILESUFFIX}3
+
+rm -f $FILE.${TMPFILESUFFIX}1 $FILE.${TMPFILESUFFIX}2
+mv $FILE.${TMPFILESUFFIX}3 $FILE.std
