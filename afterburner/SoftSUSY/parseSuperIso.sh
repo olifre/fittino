42afc7fa1a9237093dceae4c268b3fa86109bd50
Matthias Hamer <mhamer@uni-goettingen.de>
2011-08-12 13:28:41 +0000

    add afterburner scripts for SoftSUSY

---
+++ afterburner/SoftSUSY/parseSuperIso.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+bsg=`grep '# BR(b->s gamma)' ./output.flha | awk '{print $3}' | head -n 1`
+Bsmumu=`grep '# BR(B_s->mu+ mu-)' ./output.flha | awk '{print $3}' | head -n 1`
+Butaunu=`grep '# BR(B_u->tau nu)' ./output.flha | grep -v 'MA02' | awk '{print $3}' | head -n 1`
+Dstaunu=`grep '# BR(D_s->tau nu)' ./output.flha | awk '{print $3}' | head -n 1`
+Dsmunu=`grep '# BR(D_s->mu nu)' ./output.flha | awk '{print $3}' | head -n 1`
+BpD0taunu=`grep '# BR(B+->D0 tau nu)' ./output.flha | awk '{print $3}' | head -n 1`
+KmunuPimunu=`grep '# BR(K->mu nu)/BR(pi->mu nu)' ./output.flha | awk '{print $3}' | head -n 1`
+
+echo "Block SuperIso # SuperIso observables" >> susyhit_slha.out
+echo "    1    $bsg # BR(b->s gamma)" >> susyhit_slha.out
+echo "    2    $Bsmumu # BR(B_s->mu+ mu-)" >> susyhit_slha.out
+echo "    3    $Butaunu # BR(B_u->tau nu)" >> susyhit_slha.out
+echo "    4    $Dstaunu # BR(D_s->tau nu)" >> susyhit_slha.out
+echo "    5    $Dsmunu # BR(D_s->mu nu)" >> susyhit_slha.out
+echo "    6    $BpD0taunu # BR(B+->D0 tau nu)" >> susyhit_slha.out
+echo "    7    $KmunuPimunu # BR(K->mu nu)/BR(pi->mu nu)" >> susyhit_slha.out
---
+++ afterburner/SoftSUSY/removeSPhenoBlocks.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# This script removes non-standard SLHA blocks written by SPheno.
+# This is needed to feed SPheno spectrum files into programs which
+# do not ignore unknown blocks and thus do not work properly.
+
+if [ ${#*} -ne 1 ]; then
+    echo "$0: wrong number of parameters"
+    echo "usage: `basename $0` <SPheno spectrum file>"
+fi
+
+FILE=$1
+TMPFILESUFFIX=tmp
+
+STARTINFO=`cat -n $FILE | grep "Block SPhenoINFO" | awk '{ print $1}'`
+AFTERINFO=`cat -n $FILE | grep "Block MODSEL" | awk '{ print $1}'`
+ENDINFO=`expr $AFTERINFO - 1`
+
+sed "$STARTINFO,$ENDINFO d" $FILE > $FILE.${TMPFILESUFFIX}1
+
+STARTHB=`cat -n $FILE.${TMPFILESUFFIX}1 | grep "Block HiggsBoundsInputHiggsCouplingsFermions" | awk '{ print $1}'`
+AFTERHB=`cat -n $FILE.${TMPFILESUFFIX}1 | grep "Block SPhenoLowEnergy" | awk '{ print $1}'`
+ENDHB=`expr $AFTERHB - 1`
+
+sed "$STARTHB,$ENDHB d" $FILE.${TMPFILESUFFIX}1 > $FILE.${TMPFILESUFFIX}2
+
+STARTLE=`cat -n $FILE.${TMPFILESUFFIX}2 | grep "Block SPhenoLowEnergy" | awk '{ print $1}'`
+#AFTERLE=`cat -n $FILE.${TMPFILESUFFIX}2 | grep "BLOCK PREDICT" | awk '{ print $1}'`
+#ENDLE=`expr $AFTERLE - 1`
+ENDLE=`wc -l $FILE.${TMPFILESUFFIX}2 | awk '{ print $1 }'`
+
+sed "$STARTLE,$ENDLE d" $FILE.${TMPFILESUFFIX}2 > $FILE.${TMPFILESUFFIX}3
+
+rm -f $FILE.${TMPFILESUFFIX}1 $FILE.${TMPFILESUFFIX}2
+mv $FILE.${TMPFILESUFFIX}3 $FILE.stdslha
