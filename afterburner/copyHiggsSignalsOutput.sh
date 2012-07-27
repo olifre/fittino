57dbc5c002aac3ef1e1cbc8671aaafbe6322c16a
Matthias Hamer <mhamer@uni-goettingen.de>
2012-07-27 09:23:51 +0000

    update afterburner script for HiggsSignalsPeakObservables

--- afterburner/copyHiggsSignalsOutput.sh
+++ afterburner/copyHiggsSignalsOutput.sh
@@ -1,2 +1,4 @@
 cat SPheno.spc.temp.1 | grep 'HiggsSignalsResults' >> SPheno.spc
 cat SPheno.spc.temp.1 | grep 'Total Chi-squared' >> SPheno.spc 
+cat SPheno.spc.temp.1 | grep 'HiggsSignalsPeakObservables' >> SPheno.spc
+cat SPheno.spc.temp.1 | grep 'Mass and signal strength modifier of the dominant Higgs boson' >> SPheno.spc
