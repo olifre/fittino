723c7a3f426f07a5adbc9aa234e099cba612d186
Björn Sarrazin <sarrazin@physik.uni-bonn.de>
2013-02-25 23:40:56 +0000

    trying to get PP running

--- postProcessing/README/setup.sh
+++ postProcessing/README/setup.sh
@@ -4,84 +4,90 @@
 # Set general variables used by the scripts
 ################################
 
-######## Position of the setup file
-###############################
-whereIsSetup=/afs/naf.desy.de/user/p/prudent/fittino/postProcessing/README
+#Bjoern
+export fittinoPath=/afs/naf.desy.de/user/s/sarrazin/sarrazin/FittinoTrunk/
+export scratchPath=/scratch/hh/dust/naf/atlas/user/sarrazin/postProcessing_2012/
+export whereIsSetupForSed="\/afs\/naf.desy.de\/user\/s\/sarrazin\/sarrazin\/FittinoTrunk\/postProcessing\/README"
+
+#Philip
 
 
-######## Email adress
-###############################
-mymail=prudent@physik.tu-dresden.de
+#Xavier
 
-######## Plots
-###############################
 
-# Fittino repository
-fittinoPath=/afs/naf.desy.de/user/p/prudent/fittino
+#Matthias
+
+
+######################## ROOT ##########################
+export ROOTSYS=/afs/naf.desy.de/products/root/amd64_rhel50/5.34.00
+export PATH=$ROOTSYS/bin:$PATH
+export LD_LIBRARY_PATH=$ROOTSYS/lib
+
+
+####################### gcc and working dir ################
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./:/afs/naf.desy.de/group/atlas/software/lcg/external/gcc/4.3.2/lib64/
+
+######## Position of the setup file ###############################
+export whereIsSetup=$fittinoPath/postProcessing/README
+
+######## Plots ###############################
 
 # Where the batch jobs for the plots are
-plotDir=/afs/naf.desy.de/user/p/prudent/fittino/postProcessing/plots
+export plotDir=$scratchPath/jobs/plots  #moved to scratch 
 
 # Outputs of the plots
-plotOutputDir=/scratch/hh/dust/naf/atlas/user/prudent/postProcessing_2012/outputs/plots
+export plotOutputDir=$scratchPath/outputs/plots
 
-######## Toys
-###############################
+######## Toys ###############################
 
 # output of the toys
-output_TOYS=/scratch/hh/dust/naf/atlas/user/prudent/postProcessing_2012/outputs/toys/
+export output_TOYS=$scratchPath/outputs/toys/
 
 # Where all the batch-job scripts for the toys
-job_directory_TOYS=/afs/naf.desy.de/user/p/prudent/fittino/postProcessing/postProcessing_2012/jobs/jobs_for_toys/
+export job_directory_TOYS=$scratchPath/jobs/jobs_for_toys/  # move to scratch
 
 
-######## Buggy point removal
-###############################
+######## Buggy point removal ###############################
 
 # output of the buggy point removal
-output_BPR=/scratch/hh/dust/naf/atlas/user/prudent/postProcessing_2012/outputs/final/
+export output_BPR=$scratchPath/outputs/final/
 
 # Where the splines lie
-cut_directory=/afs/naf.desy.de/user/p/prudent/fittino/postProcessing/edgeDetection/
+export cut_directory=$fittinoPath/edgeDetection/
 
 # Where all the batch-job scripts for the buggy point removal
-job_directory_BPR=/afs/naf.desy.de/user/p/prudent/fittino/postProcessing/postProcessing_2012/jobs/jobs_for_buggyPointRemoval/
+export job_directory_BPR=$scratchPath/jobs/jobs_for_buggyPointRemoval/
 
 
-######## Chi2 calculation
-###############################
+######## Chi2 calculation ###############################
 
 # Output of the chi2 calculation
-output_CHI2=/scratch/hh/dust/naf/atlas/user/prudent/postProcessing_2012/outputs/
+export output_CHI2=$scratchPath/outputs/ #???? #FIXME 
 
 # Where all the batch-job scripts for the chi2 calculation lie
-job_directory_CHI2=/scratch/hh/current/atlas/users/prudent/fittino/postProcessing/postProcessing_2012/jobs/
+export job_directory_CHI2=$scratchPath/jobs/
 
 # Fittino input file with the description of all observables
-input_obs=/afs/naf.desy.de/user/p/prudent/fittino/inputs/summer2012/postProcessing
+export input_obs=$fittinoPath/inputs/summer2012/postProcessing
 
 
-######## Multiple point removal
-###############################
+######## Multiple point removal ###############################
 
 # Output of the multiple point removal
-output_MPR=/scratch/hh/current/atlas/users/prudent/fittino/postProcessing/postProcessing_2012/jobs/jobs_for_multiplePointsRemoval/doublePointsRemoved/
+export output_MPR=$scratchPath/jobs/jobs_for_multiplePointsRemoval/doublePointsRemoved/
 
 # Where all the batch-job scripts for the multiple point removal lie
-job_directory_MPR=/scratch/hh/current/atlas/users/prudent/fittino/postProcessing/postProcessing_2012/jobs/jobs_for_multiplePointsRemoval/
+export job_directory_MPR=$scratchPath/jobs/jobs_for_multiplePointsRemoval/
 
 
-
-
-######## Links to tools
-###############################
+######## Links to tools ###############################
 
 # Where all the list of the fits to be processed lie
-directory_listFits=/afs/naf.desy.de/user/p/prudent/fittino/postProcessing/README/listOfFits
+export directory_listFits=$fittinoPath/postProcessing/README/listOfFits
 
 # Where the processing programs lie
-working_directory=/afs/naf.desy.de/user/p/prudent/fittino/postProcessing
+export working_directory=$fittinoPath/postProcessing
 
 # Where to retrieve the LHC chi2 tools from
-tools_directory=/afs/naf.desy.de/user/p/prudent/fittino/tools/GetToyLHCChi2/ProfileLikelihood/
+export tools_directory=$fittinoPath/tools/GetToyLHCChi2/ProfileLikelihood/
 
