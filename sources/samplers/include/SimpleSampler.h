c78031a64894531598835f612ad54782eac3db27
Mathias Uhlenbrock <uhlenbrock@physik.uni-bonn.de>
2013-03-15 15:26:20 +0000

    Made SimpleSampler compliant with general coding style conventinons.

--- sources/samplers/include/SimpleSampler.h
+++ sources/samplers/include/SimpleSampler.h
@@ -1,13 +1,14 @@
+/* $Id: SimpleSampler.h 613 2010-05-26 09:42:00Z kullik $ */
 
 /*******************************************************************************
 *                                                                              *
 * Project     Fittino - A SUSY Parameter Fitting Package                       *
 *                                                                              *
-* File        SimpleSampler.h                                             *
+* File        SimpleSampler.h                                                  *
 *                                                                              *
-* Description                                                                  *
+* Description Class for simple parameter sampler                               *
 *                                                                              *
-* Authors                                                                      *
+* Authors     Pia Kullik  <kullik@physik.uni-bonn.de>                          *
 *                                                                              *
 * Licence     This program is free software; you can redistribute it and/or    *
 *             modify it under the terms of the GNU General Public License as   *
@@ -16,44 +17,67 @@
 *                                                                              *
 *******************************************************************************/
 
-#ifndef FITTINO_SimpleSampler_H
-#define FITTINO_SimpleSampler_H
+#ifndef FITTINO_SIMPLESAMPLER_H
+#define FITTINO_SIMPLESAMPLER_H
 
-#include "SamplerBase.h"
 #include <vector>
 
+#include "SamplerBase.h"
 
+/*!
+ *  \brief Fittino namespace.
+ */
 namespace Fittino {
 
-
+  /*!
+   *  \ingroup samplers
+   *  \brief Class for simple parameter sampler.
+   */
   class SimpleSampler : public SamplerBase {
 
     public:
+      /*!
+       *  Standard constructor.
+       */
 
-                             SimpleSampler( ModelBase* model );
+                          SimpleSampler( ModelBase* model );
+      /*!
+       *  Standard destructor.
+       */
+                          ~SimpleSampler();
 
-                             ~SimpleSampler();
-
+      /*! \cond UML */
+    private:
+      int                 _allPoints;
+      int                 _delta1Points;
+      int                 _delta2Points;
+      std::vector<double> _incrementValues;
+      std::vector<double> _lowerBoundValues;
+      std::vector<double> _upperBoundValues;
 
     private:
-      std::vector<double>    _incrementValues;
-      std::vector<double>    _upperBoundValues;
-      std::vector<double>    _lowerBoundValues;
-      int                    _delta1Points;
-      int                    _delta2Points;
-      int                    _allPoints;
+      virtual void        PrintSteeringParameters() const;
+      virtual void        ResetValues( int i );
+      /*!
+       *  Scans chi2 by iterating over a given parameter.
+       */
+      virtual void        Scan( int index );
+      /*!
+       *  Call Scan() for the last parameter to start the recursion.
+       */
+      virtual void        UpdateModel();
+      /*!
+       *  Updates within an iteration of UpdateModel():
+       *  - increase the current parameter by the desired increment
+       *  - reset all following parameters to their original values
+       *  - leave previous parameters unchanged
+      */
+      virtual void        UpdateValues( int i );
 
-    private:
-      virtual void           PrintSteeringParameters() const;
-      virtual void           UpdateModel();
-      virtual void           Scan(int index);
-      virtual void           UpdateValues(int i);
-      virtual void           ResetValues(int i);
-
+      /*! \endcond UML */
 
   };
 
 }
 
-#endif // FITTINO_SimpleSampler_H
-
+#endif // FITTINO_SIMPLESAMPLER_H
