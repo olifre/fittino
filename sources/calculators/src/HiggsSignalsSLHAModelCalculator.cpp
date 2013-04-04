b47089456bffc1dd7ec47d390ad94c2a5a35fd80
Mathias Uhlenbrock <uhlenbrock@physik.uni-bonn.de>
2013-04-04 13:26:49 +0000

    Code cleanup.

--- sources/calculators/src/HiggsSignalsSLHAModelCalculator.cpp
+++ sources/calculators/src/HiggsSignalsSLHAModelCalculator.cpp
@@ -18,8 +18,6 @@
 *******************************************************************************/
 
 #include <cmath>
-#include <cstring>
-#include <iostream>
 #include <sstream>
 
 #include "HiggsSignalsSLHAModelCalculator.h"
@@ -44,11 +42,11 @@ void Fittino::HiggsSignalsSLHAModelCalculator::Initialize() const {
 
     initialize_higgssignals_latestresults_( &nHzero, &nHplus );
 
-    int output_level = 0; //(0: silent, 1: screen output , 2: even more output )
+    int output_level = 0; // 0: silent, 1: screen output , 2: even more output
 
     setup_output_level_( &output_level );
 
-    int pdf = 2; //(1: box, 2: gaussian, 3: both)
+    int pdf = 2; // 1: box, 2: gaussian, 3: both
 
     setup_pdf_( &pdf );
 
@@ -83,7 +81,7 @@ double Fittino::HiggsSignalsSLHAModelCalculator::get_singleh_uncertainty( double
     dbbh = 0.2;
     double get_singleh_uncertainty;
 
-    if ( g2hgg <= 1.0e-16/*vsmall=1.0e-16 */  && g2hbb <= 1.0e-16/*vsmall=1.0e-16*/ ) {
+    if ( g2hgg <= 1.0e-16/* vsmall=1.0e-16 */  && g2hbb <= 1.0e-16/* vsmall=1.0e-16*/ ) {
 
         get_singleh_uncertainty = 0.0;
 
@@ -100,51 +98,55 @@ double Fittino::HiggsSignalsSLHAModelCalculator::get_singleh_uncertainty( double
 
 void Fittino::HiggsSignalsSLHAModelCalculator::CallFunction( PhysicsModelBase* model ) {
 
-    double Mh = model->GetParameterVector()->at( 0 )->GetValue();
-    double g2hjss_s = model->GetParameterVector()->at( 1 )->GetValue();
-    double g2hjss_p = model->GetParameterVector()->at( 2 )->GetValue();
-    double g2hjcc_s = model->GetParameterVector()->at( 3 )->GetValue();
-    double g2hjcc_p = model->GetParameterVector()->at( 4 )->GetValue();
-    double g2hjbb_s = model->GetParameterVector()->at( 5 )->GetValue();
-    double g2hjbb_p = model->GetParameterVector()->at( 6 )->GetValue();
-    double g2hjtt_s = model->GetParameterVector()->at( 7 )->GetValue();
-    double g2hjtt_p = model->GetParameterVector()->at( 8 )->GetValue();
-    double g2hjmumu_s = model->GetParameterVector()->at( 9 )->GetValue();
-    double g2hjmumu_p = model->GetParameterVector()->at( 10 )->GetValue();
-    double g2hjtautau_s = model->GetParameterVector()->at( 11 )->GetValue();
-    double g2hjtautau_p = model->GetParameterVector()->at( 12 )->GetValue();
-    double g2hjWW = model->GetParameterVector()->at( 13 )->GetValue();
-    double g2hjZZ = model->GetParameterVector()->at( 14 )->GetValue();
-    double g2hjZga = model->GetParameterVector()->at( 15 )->GetValue();
-    double g2hjgg = model->GetParameterVector()->at( 16 )->GetValue();
-    double g2hjggZ = model->GetParameterVector()->at( 17 )->GetValue();
-    double g2hjhiZ = model->GetParameterVector()->at( 18 )->GetValue();
+    double Mh             = model->GetParameterVector()->at(  0 )->GetValue();
+
+    double g2hjss_s       = pow( 1 + model->GetParameterVector()->at(  1 )->GetValue(), 2 );
+    double g2hjss_p       = pow( 1 + model->GetParameterVector()->at(  2 )->GetValue(), 2 );
+    double g2hjcc_s       = pow( 1 + model->GetParameterVector()->at(  3 )->GetValue(), 2 );
+    double g2hjcc_p       = pow( 1 + model->GetParameterVector()->at(  4 )->GetValue(), 2 );
+    double g2hjbb_s       = pow( 1 + model->GetParameterVector()->at(  5 )->GetValue(), 2 );
+    double g2hjbb_p       = pow( 1 + model->GetParameterVector()->at(  6 )->GetValue(), 2 );
+    double g2hjtt_s       = pow( 1 + model->GetParameterVector()->at(  7 )->GetValue(), 2 );
+    double g2hjtt_p       = pow( 1 + model->GetParameterVector()->at(  8 )->GetValue(), 2 );
+    double g2hjmumu_s     = pow( 1 + model->GetParameterVector()->at(  9 )->GetValue(), 2 );
+    double g2hjmumu_p     = pow( 1 + model->GetParameterVector()->at( 10 )->GetValue(), 2 );
+    double g2hjtautau_s   = pow( 1 + model->GetParameterVector()->at( 11 )->GetValue(), 2 );
+    double g2hjtautau_p   = pow( 1 + model->GetParameterVector()->at( 12 )->GetValue(), 2 );
+    double g2hjWW         = pow( 1 + model->GetParameterVector()->at( 13 )->GetValue(), 2 );
+    double g2hjZZ         = pow( 1 + model->GetParameterVector()->at( 14 )->GetValue(), 2 );
+    double g2hjZga        = pow( 1 + model->GetParameterVector()->at( 15 )->GetValue(), 2 );
+    double g2hjgg         = pow( 1 + model->GetParameterVector()->at( 16 )->GetValue(), 2 );
+    double g2hjggZ        = pow( 1 + model->GetParameterVector()->at( 17 )->GetValue(), 2 );
+    double g2hjhiZ        = pow( 1 + model->GetParameterVector()->at( 18 )->GetValue(), 2 );
+
+    double BR_hjhihi      = model->GetParameterVector()->at( 19 )->GetValue();
+    double BR_hjinvisible = model->GetParameterVector()->at( 20 )->GetValue();
+
     double g2hjgaga = get_g2hgaga( sqrt( g2hjbb_s ), sqrt( g2hjtt_s ), sqrt( g2hjtautau_s ), sqrt( g2hjWW ), sqrt( g2hjZZ ) );
 
-    double BR_hjhihi = 0.0, BR_hjinvisible = 0.0;
-    double SMGammaTotal;
+    double SMGammaTotal = smgamma_h_( &Mh );
 
-    SMGammaTotal = smgamma_h_( &Mh );
+    // Calculate the new total decay width.
 
-    //Calculate the new total decay width:
     double GammaTotal = SMGammaTotal * ( 1 + ( g2hjWW - 1 ) * smbr_hww_( &Mh ) + ( g2hjZZ - 1 ) * smbr_hzz_( &Mh ) +
                                          ( g2hjgg - 1 ) * smbr_hgg_( &Mh ) + ( g2hjtt_s - 1 ) * smbr_htoptop_( &Mh ) + ( g2hjbb_s - 1 ) * smbr_hbb_( &Mh ) +
                                          ( g2hjtautau_s -  1 ) * smbr_htautau_( &Mh ) + ( g2hjgaga - 1 ) * smbr_hgamgam_( &Mh ) );
 
-    //Set the (relative) rate uncertainties
+    // Set the (relative) rate uncertainties.
+
     double dCS[5], dBR[5];
 
-    dCS[0] = 0.147; //singleH
-    dCS[1] = 0.028; //VBF
-    dCS[2] = 0.037; //HW
-    dCS[3] = 0.051; //HZ
-    dCS[4] = 0.12;  //ttH
+    dCS[0] = 0.147; // singleH
+    dCS[1] = 0.028; // VBF
+    dCS[2] = 0.037; // HW
+    dCS[3] = 0.051; // HZ
+    dCS[4] = 0.12;  // ttH
 
-    dBR[0] = 0.054; //gamma gamma
-    dBR[1] = 0.048; //W W
-    dBR[2] = 0.048; //Z Z
-    dBR[3] = 0.061; //tau tau
-    dBR[4] = 0.028;  //b bbar
+    dBR[0] = 0.054; // gammagamma
+    dBR[1] = 0.048; // WW
+    dBR[2] = 0.048; // ZZ
+    dBR[3] = 0.061; // tautau
+    dBR[4] = 0.028; // bbbar
 
     double dggh = 0.147, dbbh = 0.2;
     double g2hjbb = g2hjbb_s + g2hjbb_p;
@@ -158,7 +160,7 @@ void Fittino::HiggsSignalsSLHAModelCalculator::CallFunction( PhysicsModelBase* m
                                      &g2hjWW, &g2hjZZ, &g2hjZga, &g2hjgaga, &g2hjgg, &g2hjggZ, &g2hjhiZ, &BR_hjinvisible, &BR_hjhihi );
 
     double Chisq_mu, Chisq_mh, Chisq, Pvalue;
-    int nobs, mode = 1;   //"mode" (= 1, 2, 3 for peak-centered, masse-centered chi^2 method or both)
+    int nobs, mode = 1; // mode = 1, 2, 3 for peak-centered, masse-centered chi^2 method or both
 
     run_higgssignals_( &mode, &Chisq_mu, &Chisq_mh, &Chisq, &nobs, &Pvalue );
 
@@ -168,7 +170,6 @@ void Fittino::HiggsSignalsSLHAModelCalculator::CallFunction( PhysicsModelBase* m
     std::string tmpString_Chisq_mu;
    
     tmpStream_Chisq_mu << Chisq_mu; 
-
     tmpStream_Chisq_mu >> tmpString_Chisq_mu;
 
     _slhaOutputDataStorage->AddLine( "HIGGSSIGNALSOUTPUT:1:" + tmpString_Chisq_mu + ":# Chisq_mu" );
@@ -177,7 +178,6 @@ void Fittino::HiggsSignalsSLHAModelCalculator::CallFunction( PhysicsModelBase* m
     std::string tmpString_Chisq_mh;
 
     tmpStream_Chisq_mh << Chisq_mh;                                     
-
     tmpStream_Chisq_mh >> tmpString_Chisq_mh;
 
     _slhaOutputDataStorage->AddLine( "HIGGSSIGNALSOUTPUT:2:" + tmpString_Chisq_mh + ":# Chisq_mh" );
@@ -186,7 +186,6 @@ void Fittino::HiggsSignalsSLHAModelCalculator::CallFunction( PhysicsModelBase* m
     std::string tmpString_Chisq;
 
     tmpStream_Chisq << Chisq;                                     
-
     tmpStream_Chisq >> tmpString_Chisq;
 
     _slhaOutputDataStorage->AddLine( "HIGGSSIGNALSOUTPUT:3:" + tmpString_Chisq + ":# Chisq" );
@@ -195,20 +194,11 @@ void Fittino::HiggsSignalsSLHAModelCalculator::CallFunction( PhysicsModelBase* m
     std::string tmpString_Pvalue;
 
     tmpStream_Pvalue << Pvalue;                                     
-
     tmpStream_Pvalue >> tmpString_Pvalue;
 
     _slhaOutputDataStorage->AddLine( "HIGGSSIGNALSOUTPUT:4:" + tmpString_Pvalue + ":# Pvalue" );
 
     _slhaOutputDataStorage->WriteFile("HiggsSignals.out.txt");
-
-    int ii, collider = 3; // "collider" (= 1, 2, 3 for TEV, LHC7 or LHC8)
-    int Nchannels = 1, IDchannels = 10; //Nchannels = dimension (IDchannels)
-    double singleH_rate, Htobb_rate;
-
-    //get_rates_( &ii, &collider, &Nchannels, &IDchannels, &singleH_rate );
-    //IDchannels = 15;
-    //get_rates_( &ii, &collider, &Nchannels, &IDchannels, &Htobb_rate );
 
 }
 
--- sources/models/src/HECModel.cpp
+++ sources/models/src/HECModel.cpp
@@ -25,32 +25,33 @@ Fittino::HECModel::HECModel() {
 
     _name = "HEC model";
 
-    _numberOfParameters = 20;
+    _numberOfParameters = 21;
 
-    _parameterVector.push_back( new PhysicsParameter( "Massh",             126., "GeV", 0.1, -10., 10. ) );
-    _parameterVector.push_back( new PhysicsParameter( "Delta_s_hss",         1.,    "", 0.1, -10., 10. ) );
-    _parameterVector.push_back( new PhysicsParameter( "Delta_p_hss",         0.,    "", 0.1, -10., 10. ) );
-    _parameterVector.push_back( new PhysicsParameter( "Delta_s_hcc",         1.,    "", 0.1, -10., 10. ) );
-    _parameterVector.push_back( new PhysicsParameter( "Delta_p_hcc",         0.,    "", 0.1, -10., 10. ) );
-    _parameterVector.push_back( new PhysicsParameter( "Delta_s_hbb",         1.,    "", 0.1, -10., 10. ) );
-    _parameterVector.push_back( new PhysicsParameter( "Delta_p_hbb",         0.,    "", 0.1, -10., 10. ) );
-    _parameterVector.push_back( new PhysicsParameter( "Delta_s_htt",         1.,    "", 0.1, -10., 10. ) );
-    _parameterVector.push_back( new PhysicsParameter( "Delta_p_htt",         0.,    "", 0.1, -10., 10. ) );
-    _parameterVector.push_back( new PhysicsParameter( "Delta_s_hmumu",       1.,    "", 0.1, -10., 10. ) );
-    _parameterVector.push_back( new PhysicsParameter( "Delta_p_hmumu",       0.,    "", 0.1, -10., 10. ) );
-    _parameterVector.push_back( new PhysicsParameter( "Delta_s_htautau",     1.,    "", 0.1, -10., 10. ) );
-    _parameterVector.push_back( new PhysicsParameter( "Delta_p_htautau",     0.,    "", 0.1, -10., 10. ) );
-    _parameterVector.push_back( new PhysicsParameter( "Delta_hWW",           1.,    "", 0.1, -10., 10. ) );
-    _parameterVector.push_back( new PhysicsParameter( "Delta_hZZ",           1.,    "", 0.1, -10., 10. ) );
-    _parameterVector.push_back( new PhysicsParameter( "Delta_hZgamma",       1.,    "", 0.1, -10., 10. ) );
-    _parameterVector.push_back( new PhysicsParameter( "Delta_hgg",           1.,    "", 0.1, -10., 10. ) );
-    _parameterVector.push_back( new PhysicsParameter( "Delta_hggZ",          1.,    "", 0.1, -10., 10. ) );
-    _parameterVector.push_back( new PhysicsParameter( "Delta_hihjZ",         0.,    "", 0.1, -10., 10. ) );
-    _parameterVector.push_back( new PhysicsParameter( "Delta_hgammagamma",   1.,    "", 0.1, -10., 10. ) );
+    _parameterVector.push_back( new PhysicsParameter( "Massh",           126., "GeV", 0.1, 120., 130. ) );
+    _parameterVector.push_back( new PhysicsParameter( "Delta_s_hss",       0.,    "", 0.1,  -1.,   5. ) );
+    _parameterVector.push_back( new PhysicsParameter( "Delta_p_hss",      -1.,    "", 0.1,  -1.,   5. ) );
+    _parameterVector.push_back( new PhysicsParameter( "Delta_s_hcc",       0.,    "", 0.1,  -1.,   5. ) );
+    _parameterVector.push_back( new PhysicsParameter( "Delta_p_hcc",      -1.,    "", 0.1,  -1.,   5. ) );
+    _parameterVector.push_back( new PhysicsParameter( "Delta_s_hbb",       0.,    "", 0.1,  -1.,   5. ) );
+    _parameterVector.push_back( new PhysicsParameter( "Delta_p_hbb",      -1.,    "", 0.1,  -1.,   5. ) );
+    _parameterVector.push_back( new PhysicsParameter( "Delta_s_htt",       0.,    "", 0.1,  -1.,   5. ) );
+    _parameterVector.push_back( new PhysicsParameter( "Delta_p_htt",      -1.,    "", 0.1,  -1.,   5. ) );
+    _parameterVector.push_back( new PhysicsParameter( "Delta_s_hmumu",     0.,    "", 0.1,  -1.,   5. ) );
+    _parameterVector.push_back( new PhysicsParameter( "Delta_p_hmumu",    -1.,    "", 0.1,  -1.,   5. ) );
+    _parameterVector.push_back( new PhysicsParameter( "Delta_s_htautau",   0.,    "", 0.1,  -1.,   5. ) );
+    _parameterVector.push_back( new PhysicsParameter( "Delta_p_htautau",  -1.,    "", 0.1,  -1.,   5. ) );
+    _parameterVector.push_back( new PhysicsParameter( "Delta_hWW",         0.,    "", 0.1,  -1.,   5. ) );
+    _parameterVector.push_back( new PhysicsParameter( "Delta_hZZ",         0.,    "", 0.1,  -1.,   5. ) );
+    _parameterVector.push_back( new PhysicsParameter( "Delta_hZgamma",     0.,    "", 0.1,  -1.,   5. ) );
+    _parameterVector.push_back( new PhysicsParameter( "Delta_hgg",         0.,    "", 0.1,  -1.,   5. ) );
+    _parameterVector.push_back( new PhysicsParameter( "Delta_hggZ",        0.,    "", 0.1,  -1.,   5. ) );
+    _parameterVector.push_back( new PhysicsParameter( "Delta_hihjZ",      -1.,    "", 0.1,  -1.,   5. ) );
+    _parameterVector.push_back( new PhysicsParameter( "BR_hjhihi",         0.,    "", 0.1,   0.,   1. ) );
+    _parameterVector.push_back( new PhysicsParameter( "BR_hInvisible",     0.,    "", 0.1,   0.,   1. ) );
 
-    //HiggsSignalsSLHAModelCalculator* slhaModelCalculator = new HiggsSignalsSLHAModelCalculator();
+    //SLHAModelCalculatorBase* slhaModelCalculator = new HiggsSignalsSLHAModelCalculator();
     //_modelCalculatorVector.push_back( slhaModelCalculator );
-    
+
     //_chi2ContributionVector.push_back( new SLHAChi2Contribution( "HiggsSignals", slhaModelCalculator, "HIGGSSIGNALSOUTPUT", 3, 1 ) );
 
     PhysicsModelBase::Initialize();
