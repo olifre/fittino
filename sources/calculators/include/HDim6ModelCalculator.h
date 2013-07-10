aa32a6ab191ee2e951ba4427ed48a03010c87e2d
Björn Sarrazin <sarrazin@physik.uni-bonn.de>
2013-07-10 18:14:21 +0000

    Added HDim6Calculator skeleton.

---
+++ sources/calculators/include/HDim6ModelCalculator.h
@@ -0,0 +1,60 @@
+
+
+/*******************************************************************************
+*                                                                              *
+* Project     Fittino - A SUSY Parameter Fitting Package                       *
+*                                                                              *
+* File        HDim6ModelCalculator.h                                           *
+*                                                                              *
+* Description Wrapper class for HDim6                                          *
+*                                                                              *
+* Authors     Bjoern Sarrazin  <sarrazin@physik.uni-bonn.de>                   *
+*                                                                              *
+* Licence     This program is free software; you can redistribute it and/or    *
+*             modify it under the terms of the GNU General Public License as   *
+*             published by the Free Software Foundation; either version 3 of   *
+*             the License, or (at your option) any later version.              *
+*                                                                              *
+*******************************************************************************/
+
+#ifndef FITTINO_HDIM6MODELCALCULATOR_H
+#define FITTINO_HDIM6MODELCALCULATOR_H
+
+#include "ModelCalculatorBase.h"
+
+
+/*!
+ *  \brief Fittino namespace.
+ */
+namespace Fittino {
+
+  /*!
+   *  \ingroup calculators
+   *  \brief Wrapper class for HDim6 
+   */
+  class HDim6ModelCalculator : public ModelCalculatorBase {
+
+    public:
+      /*!
+       *  Standard constructor.
+       */
+                   HDim6ModelCalculator();
+      /*!
+       *  Standard destructor.
+       */
+                   ~HDim6ModelCalculator();
+
+    public:
+      virtual void CalculatePredictions( PhysicsModelBase* model );
+      virtual void Initialize() const;
+
+    private:
+      virtual void CallExecutable();
+      virtual void CallFunction( PhysicsModelBase* model );
+      virtual void ConfigureInput( PhysicsModelBase* model );
+
+  };
+
+}
+
+#endif // FITTINO_HDIM6MODELCALCULATOR_H
---
+++ sources/calculators/src/HDim6ModelCalculator.cpp
@@ -0,0 +1,58 @@
+
+
+/*******************************************************************************
+*                                                                              *
+* Project     Fittino - A SUSY Parameter Fitting Package                       *
+*                                                                              *
+* File        HDim6ModelCalculator.cpp                                         *
+*                                                                              *
+* Description Wrapper class for HDim6                                          * 
+*                                                                              *
+* Authors     Bjoern Sarrazin  <sarrazin@physik.uni-bonn.de>                   *
+*                                                                              *
+* Licence     This program is free software; you can redistribute it and/or    *
+*             modify it under the terms of the GNU General Public License as   *
+*             published by the Free Software Foundation; either version 3 of   *
+*             the License, or (at your option) any later version.              *
+*                                                                              *
+*******************************************************************************/
+
+#include "HDim6/CrossSection.h" 
+#include "HDim6/CrossSection_had.h" 
+#include "HDim6/decay.h" 
+#include "HDim6/effinputs.h" 
+#include "HDim6/sminputs.h"
+#include "HDim6/VBF.h"
+
+#include "HDim6ModelCalculator.h"
+
+
+
+Fittino::HDim6ModelCalculator::HDim6ModelCalculator() {
+
+    _name = "HDim6ModelCalculator";
+
+}
+
+Fittino::HDim6ModelCalculator::~HDim6ModelCalculator() {
+
+}
+
+void Fittino::HDim6ModelCalculator::Initialize() const {
+
+}
+
+void Fittino::HDim6ModelCalculator::CallExecutable() {
+
+}
+
+void Fittino::HDim6ModelCalculator::CallFunction( PhysicsModelBase* model ) {
+
+  effinputs effinputs;
+
+}
+
+
+void Fittino::HDim6ModelCalculator::ConfigureInput( PhysicsModelBase* model ) {
+
+}
