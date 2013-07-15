f476bd37472aef2e79dafd6a73813c4dde6dd54e
Bastian Heller <bastian.heller@rwth-aachen.de>
2013-07-15 05:52:58 +0000

    test program added

---
+++ external/HDim6/testprog.cc
@@ -0,0 +1,125 @@
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+
+// Zur Zeitmessung
+#include <time.h>
+
+#include "CrossSection_had.h"
+#include "decay.h"
+#include "VBF.h"
+
+using namespace std;
+
+int main( int argc, char ** argv )
+{
+	if( argc != 2 )
+	{
+		cout<<"Usage: testprog <PDFSET>"<<endl;
+		return 0;
+	};
+	string pdfset = string(argv[1]);
+
+	cout<<"Using PDF-Set: "<<pdfset<<endl;
+
+	sminputs  smvalues;
+	effinputs effvalues;
+  
+	/* SM-Kopplungen und Weinberg-Winkel */
+
+	effvalues.fbb  = 0;
+	effvalues.fww  = 0;
+	effvalues.fgg  = 0;
+	effvalues.fb   = 0;
+	effvalues.fw   = 0;
+	effvalues.fuph = 0;
+	effvalues.fdoh = 0;
+	effvalues.fchh = 0;
+	effvalues.fsth = 0;
+	effvalues.ftoh = 0;
+	effvalues.fboh = 0;
+	effvalues.felh = 0;
+	effvalues.fmuh = 0;
+	effvalues.ftah = 0;
+	effvalues.ghyy = 0;
+  
+	smvalues.mel    = 0.511e-3;
+	smvalues.mmu    = 0.10565;
+	smvalues.mta    = 1.777;
+	smvalues.mup    = 0.0023;
+	smvalues.mdo    = 0.0048;
+	smvalues.mch    = 1.275;
+	smvalues.mst    = 0.095;
+	smvalues.mto    = 173.5;
+	smvalues.mbo    = 4.18;
+
+	smvalues.mz     = 91.1876;
+	smvalues.mw     = 80.385;
+	smvalues.mh     = 125;
+
+	smvalues.alphae = 0.0078186;
+	smvalues.alphas = 0.11819;
+	smvalues.sw     = 0.48079;
+
+	smvalues.vud    = 0.97483;
+	smvalues.vus    = 0.2229;
+	smvalues.vub    = 0.0036;
+	smvalues.vcd    = 0.23;
+	smvalues.vcs    = 0.97389;
+	smvalues.vcb    = 40.9e-3;
+	smvalues.vtd    = 8.4e-3;
+	smvalues.vts    = 42.9e-3;
+	smvalues.vtb    = 0.89;
+	smvalues.vev    = smvalues.mw*smvalues.sw/sqrt(4.0*3.14159*smvalues.alphae)*2.0;
+	smvalues.s      = pow( 7000,2 );
+
+	LHAPDF::initPDFSet( pdfset, LHAPDF::LHGRID, 0 );
+
+	/* Initialize run */
+	cout<<"Initialisiere Kopplungen... ";
+	update_effinputs_( &smvalues, &effvalues ); //Calculates Triple-Vertex Couplings in effvalues
+	cout<<"done"<<endl;
+	cout<<"Initialisiere VBF SM-Werte...";
+	vbf_init_cs_( &smvalues );
+	cout<<"done"<<endl;
+	cout<<"Initialisiere Hadronische CS...";
+	init_hadronic_cs_( &smvalues );
+	cout<<"done"<<endl;
+	
+	cout<<endl<<"Starte Scan..."<<endl<<endl;
+
+	cout<<setw(12)<<"fBB"<<setw(12)<<"VBF"<<setw(12)<<"VBF_ERR"<<setw(12)<<"VBF_CHI";
+	cout<<setw(12)<<"bg-hb"<<setw(12)<<"bg-hb-ERR"<<setw(12)<<"bg-hb-CHI";
+        cout<<setw(12)<<"HW"<<setw(12)<<"HW_ERR"<<setw(12)<<"HW_CHI";
+	cout<<setw(12)<<"HZ"<<setw(12)<<"HZ_ERR"<<setw(12)<<"HZ_CHI"<<"Timecons.(s)"<<endl;
+
+	for( int i = 0; i <= 10; i++ )
+	{
+	  clock_t start, end;
+	  start = clock();
+	  effvalues.fboh = (1.0e-10)*pow( 10.0 ,(double)i );
+	  update_effinputs_( &smvalues, &effvalues );
+	  
+	  double vbfratio, hzratio, hwratio, Brhww, Brhzz, bgratio;
+	  double err_vbfratio, err_hzratio, err_hwratio, err_Brhww, err_Brhzz, err_bgratio;
+	  double chi_vbfratio, chi_hzratio, chi_hwratio, chi_Brhww, chi_Brhzz, chi_bgratio;
+
+	  cout<<scientific<<setprecision(4)<<setw(12)<<effvalues.fbb;
+
+	  initeffwidths_( &smvalues, &effvalues );
+	  ratio_vbf_2flav_( &smvalues, &effvalues, &vbfratio, &err_vbfratio, &chi_vbfratio );
+ 	  ratio_pphz_( &smvalues, &effvalues, &hzratio, &err_hzratio, &chi_hzratio );
+          ratio_pphw_( &smvalues, &effvalues, &hwratio, &err_hwratio, &chi_hwratio );
+	  ratio_bg_bh_(&smvalues, &effvalues, &bgratio, &err_bgratio, &chi_bgratio );
+	  cout<<setprecision(4)<<scientific<<setw(12)<<vbfratio<<setw(12)<<err_vbfratio<<setw(12)<<chi_vbfratio;
+	  cout<<setprecision(4)<<scientific<<setw(12)<<bgratio<<setw(12)<<err_bgratio<<setw(12)<<chi_bgratio;
+	  cout<<setprecision(4)<<scientific<<setw(12)<<hwratio<<setw(12)<<err_hwratio<<setw(12)<<chi_hwratio;
+	  cout<<setprecision(4)<<scientific<<setw(12)<<hzratio<<setw(12)<<err_hzratio<<setw(12)<<chi_hzratio;
+	  end = clock();
+	  cout<<setw(12)<<(end-start)/CLOCKS_PER_SEC<<endl;
+	};
+	
+
+	return 0;
+};
