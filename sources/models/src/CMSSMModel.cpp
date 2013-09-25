a19e24ce73a05cb7b7d7054a520ab5d45efb1d81
Matthias Hamer <mhamer@uni-goettingen.de>
2013-09-25 16:11:33 +0000

    little work on CMSSModel: can now be configured to run with a TreeSampler (nothing happens, yet); also implemented a first attempt at working with boost's property tree when reading the configuration

--- sources/models/src/CMSSMModel.cpp
+++ sources/models/src/CMSSMModel.cpp
@@ -16,6 +16,9 @@
 *	      the License, or (at your option) any later version.              *
 *                                                                              *
 *******************************************************************************/
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/foreach.hpp>
 
 #include "Configuration.h"
 #include "CMSSMModel.h"
@@ -23,6 +26,7 @@
 #include "SLHAParameter.h"
 #include "SLHAPrediction.h"
 #include "SPhenoSLHAModelCalculator.h"
+#include "TreeCalculator.h"
 
 Fittino::CMSSMModel::CMSSMModel() {
 
@@ -30,53 +34,62 @@ Fittino::CMSSMModel::CMSSMModel() {
 
     _name = "CMSSM model";
 
-    AddParameter( new SLHAParameter( "P_A0", "A_{0}",
-				     configuration->GetSteeringParameter( "A0", 0. ),
-				     "GeV", "GeV",
-				     1., -1.e5, 1.e5, -1.e5, 1.e5, "5" ) );
+    const boost::property_tree::ptree* propertyTree = configuration->GetPropertyTree();
+    if( propertyTree->get<std::string>( "InputFile.Sampler.<xmlattr>.SamplerType" ) == "Tree" ) {
 
-    AddParameter( new SLHAParameter( "P_M0", "M_{0}",
-				     configuration->GetSteeringParameter( "M0", 0. ),
-				     "GeV", "GeV",
-				     1., -1.e5, 1.e5, 0., 2300., "1" ) );
+        TreeCalculator *treeCalculator = new TreeCalculator( this, propertyTree->get<std::string>( "InputFile.Sampler.<xmlattr>.InputFileName" ), propertyTree->get<std::string>( "InputFile.Sampler.<xmlattr>.InputTreeName" ) );
+        _modelCalculatorVector.push_back( treeCalculator );
+        PhysicsModelBase::Initialize();
+    }
+    else {
 
-    AddParameter( new SLHAParameter( "P_M12", "M_{1/2}",
-				     configuration->GetSteeringParameter( "M12", 0. ),
-				     "GeV", "GeV",
-				     1., -1.e5, 1.e5,  500., 3500., "2" ) );
+        AddParameter( new SLHAParameter( "P_A0", "A_{0}",
+                                         configuration->GetSteeringParameter( "A0", 0. ),
+                                         "GeV", "GeV",
+                                         1., -1.e5, 1.e5, -1.e5, 1.e5, "5" ) );
 
-    AddParameter( new SLHAParameter( "P_TanBeta", "tan#beta",
-				     configuration->GetSteeringParameter( "TanBeta", 0. ),
-				     "", "",
-				     1., 0., 1.e3, 0., 1.e3, "3" ) );
+        AddParameter( new SLHAParameter( "P_M0", "M_{0}",
+                                         configuration->GetSteeringParameter( "M0", 0. ),
+                                         "GeV", "GeV",
+                                         1., -1.e5, 1.e5, 0., 2300., "1" ) );
 
-    SPhenoSLHAModelCalculator* slhaModelCalculator = new SPhenoSLHAModelCalculator( this );
-    _modelCalculatorVector.push_back( slhaModelCalculator );
+        AddParameter( new SLHAParameter( "P_M12", "M_{1/2}",
+                                         configuration->GetSteeringParameter( "M12", 0. ),
+                                         "GeV", "GeV",
+                                         1., -1.e5, 1.e5,  500., 3500., "2" ) );
 
-    _observableVector.push_back( new Observable( new SLHAPrediction( "O_Bsg_npf", "BR(b#rightarrows#gamma)",
-                                                                     "", "",
-                                                                     0., 1.e6,
-                                                                     slhaModelCalculator,
-                                                                     "SPhenoLowEnergy",
-                                                                     " 1", 1 ),
-                                                                     3.55e-04, 0.53e-04 ) );
+        AddParameter( new SLHAParameter( "P_TanBeta", "tan#beta",
+                                         configuration->GetSteeringParameter( "TanBeta", 0. ),
+                                         "", "",
+                                         1., 0., 1.e3, 0., 1.e3, "3" ) );
 
-    _predictionVector.push_back( new SLHAPrediction( "O_massNeutralino1", "m_{#chi_{1}^{0}}",
-                                                     "GeV", "GeV",
-                                                     5., 1500.,
-                                                     slhaModelCalculator,
-                                                     "mass",
-                                                     "dummy", 0 ) );
+        SPhenoSLHAModelCalculator* slhaModelCalculator = new SPhenoSLHAModelCalculator( this );
+        _modelCalculatorVector.push_back( slhaModelCalculator );
 
-    _predictionVector.push_back( new SLHAPrediction( "af_direct", "#sigma^{SI}",
-                                                     "pb", "pb",
-                                                     1.e-12, 1.e-2 ,
-                                                     slhaModelCalculator,
-                                                     "dummy",
-                                                     "dummy", 0 ) );
+        _observableVector.push_back( new Observable( new SLHAPrediction( "O_Bsg_npf", "BR(b#rightarrows#gamma)",
+                                     "", "",
+                                     0., 1.e6,
+                                     slhaModelCalculator,
+                                     "SPhenoLowEnergy",
+                                     " 1", 1 ),
+                                     3.55e-04, 0.53e-04 ) );
 
-    PhysicsModelBase::Initialize();
+        _predictionVector.push_back( new SLHAPrediction( "O_massNeutralino1", "m_{#chi_{1}^{0}}",
+                                     "GeV", "GeV",
+                                     5., 1500.,
+                                     slhaModelCalculator,
+                                     "mass",
+                                     "dummy", 0 ) );
 
+        _predictionVector.push_back( new SLHAPrediction( "af_direct", "#sigma^{SI}",
+                                     "pb", "pb",
+                                     1.e-12, 1.e-2 ,
+                                     slhaModelCalculator,
+                                     "dummy",
+                                     "dummy", 0 ) );
+
+        PhysicsModelBase::Initialize();
+    }
 }
 
 Fittino::CMSSMModel::~CMSSMModel() {
