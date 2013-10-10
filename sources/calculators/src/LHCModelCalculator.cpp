1006b30a9b64f77c03580d4f5db036c8448dbacc
Matthias Hamer <mhamer@uni-goettingen.de>
2013-10-10 18:03:16 +0000

    implement an algorithm for n-dimensional interpolation to be used with THnSparses for n-dimensional LHC grids...still testing this

--- sources/calculators/src/LHCModelCalculator.cpp
+++ sources/calculators/src/LHCModelCalculator.cpp
@@ -103,12 +103,77 @@ void Fittino::LHCModelCalculator::CalculatePredictions() {
   }
 
   for( std::map<std::string, THnSparseD*>::const_iterator itr = _chi2HistogramsnD.begin(); itr != _chi2HistogramsnD.end(); ++itr ) {
+    double chi2Value = 0.;
+    //std::vector<int> bins;
+    //for( int i = 0; i < _relevantParametersMap.at((*itr).first).size(); ++i ) {
+    //  bins.push_back( (*itr).second->GetAxis( i )->FindFixBin( _model->GetCollectionOfParameters().At( _relevantParametersMap.at((*itr).first).at(i))->GetValue() ) );
+    //}
+    //_simpleOutputDataStorage->GetMap()->at( (*itr).first ) = (*itr).second -> GetBinContent( &bins.at(0) );
     
-    std::vector<int> bins;
-    for( int i = 0; i < _relevantParametersMap.at((*itr).first).size(); ++i ) {
-      bins.push_back( (*itr).second->GetAxis( i )->FindFixBin( _model->GetCollectionOfParameters().At( _relevantParametersMap.at((*itr).first).at(i))->GetValue() ) );
+    /* have to do the interpolation by hand? Find out how to do this better... */
+    // first step: Find 2^n closesst points.
+    std::vector<double> parameterValues;
+    for( unsigned int i = 0; i < _relevantParametersMap.at( (*itr).first).size(); ++i ) {
+      parameterValues.push_back( _model->GetCollectionOfParameters().At( _relevantParametersMap.at((*itr).first).at(i))->GetValue() );
     }
-    _simpleOutputDataStorage->GetMap()->at( (*itr).first ) = (*itr).second -> GetBinContent( &bins.at(0) );
+
+    std::vector<double> closestHigherValues;
+    std::vector<double> closestLowerValues;
+    for( unsigned int i = 0; i < parameterValues.size(); ++i ) {
+
+      bool overFlow = false;
+      double maxValue = (*itr).second->GetAxis( i )->GetBinCenter( (*itr).second->GetAxis( i )->GetNbins() );
+      double minValue = (*itr).second->GetAxis( i )->GetBinCenter( 1 );
+      if( parameterValues.at(i) > maxValue ) { parameterValues.at(i) = maxValue; overFlow = true; }
+      if( parameterValues.at(i) < minValue ) { parameterValues.at(i) = minValue; }
+
+      int binIndex = (*itr).second->GetAxis( i )->FindFixBin( parameterValues.at(i) );
+      double binCenter = (*itr).second->GetAxis( i )->GetBinCenter( binIndex );
+      
+      if( binCenter >= parameterValues.at(i) && !overFlow ) {
+        closestHigherValues.push_back((*itr).second->GetAxis( i )->GetBinCenter(binIndex+1));
+        closestLowerValues.push_back((*itr).second->GetAxis( i )->GetBinCenter(binIndex));
+      }
+      else {
+        closestHigherValues.push_back((*itr).second->GetAxis( i )->GetBinCenter(binIndex));
+        closestLowerValues.push_back((*itr).second->GetAxis( i )->GetBinCenter(binIndex-1));
+      }
+        
+    }
+    
+    // now have the parameter value; now store the chi2 values:
+    
+    std::vector<double> chi2Values;
+    std::vector<double> chi2ValuesTemp;
+    std::vector<std::vector<double> > binaryLowerHigher;
+    binaryLowerHigher.push_back( closestLowerValues );
+    binaryLowerHigher.push_back( closestHigherValues );
+    std::vector<int> position( closestHigherValues.size(), 1 );
+    std::vector<int> coordinates( closestHigherValues.size(), 1);
+    
+    for( unsigned int i = 0; i < pow(2, closestHigherValues.size()); ++i ) {
+      for( unsigned int j = 0; j < closestHigherValues.size(); ++j ) {
+        int period = pow(2, j );
+        if( i%period == 0 ) {
+            position.at(j) = (position.at(j)+1)%2;
+        }
+        coordinates.at(j) = (*itr).second->GetAxis(j)->FindFixBin( binaryLowerHigher.at(position.at(j)).at(j));
+      }
+      chi2Values.push_back( (*itr).second->GetBinContent( &coordinates.at(0) ));
+    }
+    int interpolateCoordinate = 0;
+    while( interpolateCoordinate < closestHigherValues.size() ) {
+      chi2ValuesTemp.clear();
+      for( unsigned int i = 0; i < chi2Values.size(); ++i ) {
+        double chi2_inter = chi2Values.at(i)+ (chi2Values.at(i)-chi2Values.at(++i))/(closestLowerValues.at(interpolateCoordinate)-closestHigherValues.at(interpolateCoordinate))*(parameterValues.at(interpolateCoordinate)-closestLowerValues.at(interpolateCoordinate));
+        chi2ValuesTemp.push_back(chi2_inter);
+      }
+      chi2Values.clear();
+      for( unsigned int i = 0; i < chi2ValuesTemp.size(); ++i ) {
+        chi2Values.at(i) = chi2ValuesTemp.at(i);
+      }
+      interpolateCoordinate += 1;
+    }
   }
 
 }
