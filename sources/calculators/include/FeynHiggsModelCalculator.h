5eb974a23c1b7721b5be000274ff4fc61a85e2f5
Björn Sarrazin <sarrazin@physik.uni-bonn.de>
2013-12-06 13:29:08 +0000

    Updated FeynHiggsCalculator according to new prediction model. Can now be configured to write and use any native FH input file. Error handling still missing.

--- sources/calculators/include/FeynHiggsModelCalculator.h
+++ sources/calculators/include/FeynHiggsModelCalculator.h
@@ -55,12 +55,12 @@ namespace Fittino {
       /*!
        *  Standard constructor.
        */
-    FeynHiggsModelCalculator( const PhysicsModelBase* model, const boost::property_tree::ptree& ptree );
+      FeynHiggsModelCalculator( const PhysicsModelBase* model, const boost::property_tree::ptree& ptree );
       /*!
        *  Standard destructor.
        */
-                   ~FeynHiggsModelCalculator();
-
+      ~FeynHiggsModelCalculator();
+                   
       virtual void CalculatePredictions();
       virtual void Initialize() const;
 
@@ -70,157 +70,29 @@ namespace Fittino {
       virtual void CallFunction();
       void         ConfigureInput();
 
-    private:
-      void         Couplings();
-      void         HiggsCorr();
-      void         Production();
-      void         SetParameters();
-      void         SetSMParameters();
-
     private:  
       int    _error;
-      double _derror;
-      
-      double _alphaem_inv;
-      double _alphas_mass_Z;
-      double _GF;
-    
-      double _mass_e;
-      double _mass_mu;
-      double _mass_tau;
-
-      double _mass_u_2GeV;
-      double _mass_d_2GeV;
-      double _mass_s_2GeV;
-      double _mass_c_mass_c;
-      double _mass_b_mass_b;
-
-      double _mass_W;
-      double _mass_Z;
-
-      double _CKM_lambda;
-      double _CKM_A;
-      double _CKM_rhobar;
-      double _CKM_etabar;
-
-      double _mass_t;
-
-      double _m_susy;
-
-      double _m_1;
-      double _m_2;
-      double _m_3;
-
-      double _m_sl_1;
-      double _m_sl_2;
-      double _m_sl_3;
-
-      double _m_se_1;
-      double _m_se_2;
-      double _m_se_3;
-    
-      double _m_sq_1;
-      double _m_sq_2;
-      double _m_sq_3;
-
-      double _m_su_1;
-      double _m_su_2;
-      double _m_su_3;
-
-      double _m_sd_1;
-      double _m_sd_2;
-      double _m_sd_3;
-
-      double _mass_A0;
-      double _mass_Hp;
-
-      double _Re_mu;
-      double _Im_mu;
-      std::complex<double> _mu;
-
-      double _tanBeta;
-
-      double _Re_A_t;
-      double _Im_A_t;
-      std::complex<double> _A_t;
-
-      double _Re_A_b;
-      double _Im_A_b;
-      std::complex<double> _A_b;
-
-      double _Re_A_c;
-      double _Im_A_c;
-      std::complex<double> _A_c;
-
-      double _Re_A_s;
-      double _Im_A_s;
-      std::complex<double> _A_s;
-
-      double _Re_A_d;
-      double _Im_A_d;
-      std::complex<double> _A_d;
-
-      double _Re_A_u;
-      double _Im_A_u;
-      std::complex<double> _A_u;
-
-      double _Re_A_tau;
-      double _Im_A_tau;
-      std::complex<double> _A_tau;
-
-      double _Re_A_mu;
-      double _Im_A_mu;
-      std::complex<double> _A_mu;
-
-      double _Re_A_e;
-      double _Im_A_e;
-      std::complex<double> _A_e;
-
-      double _Q_tau;
-      double _Q_t;
-      double _Q_b;
-
-      double _scalefactor;
 
       double _m_h;
 
-      double _sigma_ggh_smratio;
-      double _sigma_ggh2_smratio;
-      double _sigma_bbh_smratio;
-      double _sigma_qqh_smratio;
-      double _sigma_tth_smratio;
-      double _sigma_Wh_smratio;
-      double _sigma_Zh_smratio;
+      double _normSM_sigma_ggh;
+      double _normSM_sigma_ggh_2;
+      double _normSM_sigma_bbh;
+      double _normSM_sigma_qqh;
+      double _normSM_sigma_tth;
+      double _normSM_sigma_Wh;
+      double _normSM_sigma_Zh;
 
-      double _gamma_h_tautau;
-      double _gamma_h_tautau_sm;
-      double _gamma_h_tautau_smratio;
-  
-      double _gamma_h_cc;
-      double _gamma_h_cc_sm;
-      double _gamma_h_cc_smratio;
-  
-      double _gamma_h_ss;
-      double _gamma_h_ss_sm;
-      double _gamma_h_ss_smratio;
-  
-      double _gamma_h_bb;
-      double _gamma_h_bb_sm;
-      double _gamma_h_bb_smratio;
-
-      double _gamma_h_gaga;
-      double _gamma_h_gaga_sm;
-      double _gamma_h_gaga_smratio;
-
-      double _gamma_h_Zga_smratio;
-
-      double _gamma_h_ZZ_smratio;
-  
-      double _gamma_h_WW_smratio;
-
-      double _gamma_h_gg_smratio;
-
-      double _gamma_h_tot_smratio;
+      double _normSM_Gamma_h_tau_tau;
+      double _normSM_Gamma_h_c_c;
+      double _normSM_Gamma_h_s_s;
+      double _normSM_Gamma_h_b_b;
+      double _normSM_Gamma_h_gamma_gamma;
+      double _normSM_Gamma_h_Z_gamma;
+      double _normSM_Gamma_h_Z_Z;
+      double _normSM_Gamma_h_W_W;
+      double _normSM_Gamma_h_g_g;
+      double _normSM_Gamma_h_total;
       
       Collection<SimplePrediction*> _input;
       
--- sources/calculators/src/FeynHiggsModelCalculator.cpp
+++ sources/calculators/src/FeynHiggsModelCalculator.cpp
@@ -18,13 +18,17 @@
 *******************************************************************************/
 
 #include <iostream>
+#include <fstream>
 
 #include <boost/property_tree/ptree.hpp>
 #include <boost/foreach.hpp>
+#include "boost/format.hpp"
+#include <boost/filesystem.hpp>
 
 #include "TMath.h"
 
 #include "CFeynHiggs.h"
+#include "CSLHA.h"
 
 #include "FeynHiggsModelCalculator.h"
 #include "ModelParameterBase.h"
@@ -32,22 +36,24 @@
 #include "SimplePrediction.h"
 
 Fittino::FeynHiggsModelCalculator::FeynHiggsModelCalculator( const PhysicsModelBase* model, const boost::property_tree::ptree& ptree )
-        : ModelCalculatorBase(model) {
+        : ModelCalculatorBase( model ) {
 
     _name = "FeynHiggs";
 
     std::string flags = "400242110";
 
-    FHSetFlagsString(&_error,flags.c_str() );
-    if ( _error ) exit(_error); 
+    FHSetFlagsString( &_error, flags.c_str() );
 
+    if ( _error != 0 ) {
+
+    }
 
     BOOST_FOREACH( const boost::property_tree::ptree::value_type& node, ptree ) {
     
-      if( node.first == "Input" ) {
+      if( node.first == "Parameter" ) {
 
-        std::string name     = node.second.get<std::string>( "Name" );
-        std::string quantity = node.second.get<std::string>( "Value" );
+        std::string name     = node.second.get<std::string>( "Name"     );
+        std::string quantity = node.second.get<std::string>( "Quantity" );
 
         const double& value = _model->GetCollectionOfQuantities().At( quantity )->GetValue();
         // std::string   unit  = _model->GetCollectionOfQuantities().At( quantity )->GetUnit(); Quantity does not yet have a unit
@@ -57,6 +63,24 @@ Fittino::FeynHiggsModelCalculator::FeynHiggsModelCalculator( const PhysicsModelB
       }
 
     }
+    
+    AddQuantity( new SimplePrediction( "NormSM_Gamma_h_gamma_gamma", "", _normSM_Gamma_h_gamma_gamma ) );  
+    AddQuantity( new SimplePrediction( "NormSM_Gamma_h_Z_gamma"    , "", _normSM_Gamma_h_Z_gamma     ) );      
+    AddQuantity( new SimplePrediction( "NormSM_Gamma_h_Z_Z"        , "", _normSM_Gamma_h_Z_Z         ) );         
+    AddQuantity( new SimplePrediction( "NormSM_Gamma_h_W_W"        , "", _normSM_Gamma_h_W_W         ) );        
+    AddQuantity( new SimplePrediction( "NormSM_Gamma_h_g_g"        , "", _normSM_Gamma_h_g_g         ) );        
+    AddQuantity( new SimplePrediction( "NormSM_Gamma_h_tau_tau"    , "", _normSM_Gamma_h_tau_tau     ) );    
+    AddQuantity( new SimplePrediction( "NormSM_Gamma_h_c_c"        , "", _normSM_Gamma_h_c_c         ) );         
+    AddQuantity( new SimplePrediction( "NormSM_Gamma_h_s_s"        , "", _normSM_Gamma_h_s_s         ) );         
+    AddQuantity( new SimplePrediction( "NormSM_Gamma_h_b_b"        , "", _normSM_Gamma_h_b_b         ) );         
+    AddQuantity( new SimplePrediction( "NormSM_Gamma_h_total"      , "", _normSM_Gamma_h_total       ) );       
+    AddQuantity( new SimplePrediction( "NormSM_sigma_ggh"          , "", _normSM_sigma_ggh           ) );          
+    AddQuantity( new SimplePrediction( "NormSM_sigma_ggh_2"        , "", _normSM_sigma_ggh_2         ) );         
+    AddQuantity( new SimplePrediction( "NormSM_sigma_bbh"          , "", _normSM_sigma_bbh           ) );          
+    AddQuantity( new SimplePrediction( "NormSM_sigma_qqh"          , "", _normSM_sigma_qqh           ) );          
+    AddQuantity( new SimplePrediction( "NormSM_sigma_tth"          , "", _normSM_sigma_tth           ) );          
+    AddQuantity( new SimplePrediction( "NormSM_sigma_Wh"           , "", _normSM_sigma_Wh            ) );           
+    AddQuantity( new SimplePrediction( "NormSM_sigma_Zh"           , "", _normSM_sigma_Zh            ) );
 
 }
 
@@ -66,51 +90,121 @@ Fittino::FeynHiggsModelCalculator::~FeynHiggsModelCalculator() {
 
 void Fittino::FeynHiggsModelCalculator::CalculatePredictions() {
 
-    std::cout<<"Calculate Predictions"<<std::endl;
+    std::string fileName = "FeynHiggs.in";
 
-    _derror = 0;
+    if ( boost::filesystem::exists( fileName ) ) {
 
-    SetSMParameters();
+        boost::filesystem::rename( fileName, fileName + ".last" );
 
-    if ( _derror ) {
+    }
+   
+    std::ofstream file( fileName.c_str() );
 
-      return;
+    for ( int i = 0; i < _input.GetNumberOfElements(); i++ ) {
+
+        file<<boost::format( "%-25s % 25.16e\n" ) % _input.At( i )->GetName() % _input.At( i )->GetValue();
 
     }
 
-    SetParameters();
+    file.close();
+    
+    int         fast = 0;
+    double      sqrts = 8;
+    ComplexType SAeff;
+    ComplexType UHiggs     [3][3];
+    ComplexType ZHiggs     [3][3];
+    ComplexType couplings  [ncouplings];
+    ComplexType couplingsms[ncouplingsms];
+    RealType    MHiggs     [4];
+    RealType    gammas     [ngammas];
+    RealType    gammasms   [ngammasms];
+    RealType    record     [nrecord];
+    RealType    prodxs     [nprodxs];
+    COMPLEX     slhadata   [nslhadata];
 
-    if ( _derror ) {
+    FHReadRecord( &_error, record, slhadata, fileName.c_str() ); 
 
-      return;
+    if ( _error != 2 ) {
+      
+    }
+
+    FHLoopRecord( &_error, record                             );
+
+    if ( _error != 0 ) {
 
     }
 
-    HiggsCorr();
 
-    if ( _derror ) {
+    FHSetRecord ( &_error, record                             );
 
-        return;
+    if ( _error != 0 ) {
+
+    }
+    
+    FHHiggsCorr ( &_error, MHiggs, &SAeff, UHiggs, ZHiggs     );
+
+
+    if ( _error != 0 ) {
 
     }
 
-    Couplings();
+    //FHSelectUZ(&_error, 1, 1, 1); // U
+    FHSelectUZ( &_error, 1, 2, 1 ); // Z
 
-    if ( _derror ) {
-
-        return;
-
-    }
-
-    Production();
-
-    if ( _derror ) {
-
-        std::cout<<"ABORT"<<std::endl;
-        return;
+    if ( _error != 0 ) {
 
     }
 
+
+    _m_h = MHiggs[0];
+
+    if ( _m_h < 1. ) {
+
+        std::cout<<"Problem in mh calculation"<<std::endl;
+
+    }
+
+    FHCouplings( &_error, couplings, couplingsms, gammas, gammasms, fast );
+    
+    if ( _error != 0 ) {
+
+    }
+
+
+
+    FHHiggsProd( &_error, sqrts, prodxs );
+
+    if ( _error != 0 ) {
+
+    }
+        
+
+    _normSM_Gamma_h_gamma_gamma = Gamma   ( H0VV( 1, 1       ) ) / GammaSM   ( H0VV( 1, 1       ) );
+    _normSM_Gamma_h_Z_gamma     = Gamma   ( H0VV( 1, 2       ) ) / GammaSM   ( H0VV( 1, 2       ) );
+    _normSM_Gamma_h_Z_Z         = Gamma   ( H0VV( 1, 3       ) ) / GammaSM   ( H0VV( 1, 3       ) );
+    _normSM_Gamma_h_W_W         = Gamma   ( H0VV( 1, 4       ) ) / GammaSM   ( H0VV( 1, 4       ) );
+    _normSM_Gamma_h_g_g         = Gamma   ( H0VV( 1, 5       ) ) / GammaSM   ( H0VV( 1, 5       ) );
+    _normSM_Gamma_h_tau_tau     = Gamma   ( H0FF( 1, 2, 3, 3 ) ) / GammaSM   ( H0FF( 1, 2, 3, 3 ) );
+    _normSM_Gamma_h_c_c         = Gamma   ( H0FF( 1, 3, 2, 2 ) ) / GammaSM   ( H0FF( 1, 3, 2, 2 ) );
+    _normSM_Gamma_h_s_s         = Gamma   ( H0FF( 1, 4, 2, 2 ) ) / GammaSM   ( H0FF( 1, 4, 2, 2 ) ); 
+    _normSM_Gamma_h_b_b         = Gamma   ( H0FF( 1, 4, 3, 3 ) ) / GammaSM   ( H0FF( 1, 4, 3, 3 ) );
+    _normSM_Gamma_h_total       = GammaTot( 1                  ) / GammaSMTot( 1                  );
+    _normSM_sigma_ggh           = ggh     ( 1                  ) / gghSM     ( 1                  ); 
+    _normSM_sigma_ggh_2         = ggh2    ( 1                  ) / gghSM     ( 1                  );
+    _normSM_sigma_bbh           = bbh     ( 1                  ) / bbhSM     ( 1                  );
+    _normSM_sigma_qqh           = qqh     ( 1                  ) / qqhSM     ( 1                  );
+    _normSM_sigma_tth           = tth     ( 1                  ) / tthSM     ( 1                  );
+    _normSM_sigma_Wh            = Wh      ( 1                  ) / WhSM      ( 1                  ); 
+    _normSM_sigma_Zh            = Zh      ( 1                  ) / ZhSM      ( 1                  ); 
+
+
+    FHLoopRecord( &_error, record                             );
+
+    if ( _error != -1 ) {
+      
+    }
+
+  
 }
 
 void Fittino::FeynHiggsModelCalculator::Initialize() const {
@@ -129,247 +223,3 @@ void Fittino::FeynHiggsModelCalculator::ConfigureInput() {
 
 }
 
-void Fittino::FeynHiggsModelCalculator::Couplings(){
-  
-    int fast=0;
-    ComplexType couplings[ncouplings];
-    ComplexType couplingsms[ncouplingsms];
-    RealType gammas[ngammas];
-    RealType gammasms[ngammasms];
-
-    int one=1;
-
-    //FHSelectUZ(&_error, one,one,one); // U
-
-    FHSelectUZ(&_error, one,2,one); // Z
-
-    FHCouplings(&_error, couplings, couplingsms, gammas, gammasms, fast);
-    _derror = _error;
-    if( _error ) return;
-
-    //  Gamma(H0FF(1,4,3,3))=666;
-
-    _gamma_h_tautau = Gamma(H0FF(1,2,3,3));
-    _gamma_h_tautau_sm = GammaSM(H0FF(1,2,3,3));
-    _gamma_h_tautau_smratio = _gamma_h_tautau / _gamma_h_tautau_sm;
-  
-    _gamma_h_cc = Gamma(H0FF(1,3,2,2));
-    _gamma_h_cc_sm = GammaSM(H0FF(1,3,2,2));
-    _gamma_h_cc_smratio = _gamma_h_cc / _gamma_h_cc_sm;
-  
-    _gamma_h_ss = Gamma(H0FF(1,4,2,2));
-    _gamma_h_ss_sm = GammaSM(H0FF(1,4,2,2));
-    _gamma_h_ss_smratio = _gamma_h_ss / _gamma_h_ss_sm;
-  
-    _gamma_h_bb = Gamma(H0FF(1,4,3,3));
-    _gamma_h_bb_sm = GammaSM(H0FF(1,4,3,3));
-    _gamma_h_bb_smratio = _gamma_h_bb / _gamma_h_bb_sm;
-
-    _gamma_h_gaga = Gamma(H0VV(1,1));
-    _gamma_h_gaga_sm = GammaSM(H0VV(1,1));
-    _gamma_h_gaga_smratio = _gamma_h_gaga / _gamma_h_gaga_sm;
-
-    _gamma_h_Zga_smratio = Gamma(H0VV(1,2))/GammaSM(H0VV(1,2));
-
-    _gamma_h_ZZ_smratio = Gamma(H0VV(1,3))/GammaSM(H0VV(1,3));
-  
-    _gamma_h_WW_smratio = Gamma(H0VV(1,4))/GammaSM(H0VV(1,4));
-
-    _gamma_h_gg_smratio = Gamma(H0VV(1,5))/GammaSM(H0VV(1,5));
-
-    _gamma_h_tot_smratio = GammaTot(1)/GammaSMTot(1);
-
-}
-
-void Fittino::FeynHiggsModelCalculator::HiggsCorr(){
-
-    RealType MHiggs[4];
-    ComplexType SAeff, UHiggs[3][3], ZHiggs[3][3];
-
-    FHHiggsCorr(&_error, MHiggs, &SAeff, UHiggs, ZHiggs);
-    _derror = _error;
-    if( _error ) return;
-
-        _m_h = MHiggs[0];
-
-    if ( _m_h < 1. ) {
-
-        std::cout<<"Problem in mh calculation"<<std::endl;
-        _error=1;
-        _derror=1;
-
-  }
-
-}
-
-void Fittino::FeynHiggsModelCalculator::Production(){
-  
-    RealType prodxs[nprodxs];
-    double sqrts=8;
-  
-    FHHiggsProd(&_error, sqrts, prodxs);
-    _derror = _error;
-
-    if( _error ) return;
-
-    _sigma_ggh_smratio = ggh(1) / gghSM(1); 
-
-    _sigma_ggh2_smratio = ggh2(1) / gghSM(1); ;
-    _sigma_bbh_smratio =  bbh(1) / bbhSM(1); ;
-    _sigma_qqh_smratio = qqh(1) / qqhSM(1); ;
-    _sigma_tth_smratio = tth(1) / tthSM(1); ;
-    _sigma_Wh_smratio = Wh(1) / WhSM(1); ;
-    _sigma_Zh_smratio = Zh(1) / ZhSM(1); ;
-  
-}
-
-void Fittino::FeynHiggsModelCalculator::SetParameters(){
-
-    _mass_t        = 173;
-
-    _m_susy        = _model->GetCollectionOfParameters().At("MSusy")->GetValue();
-
-    _m_1           = _m_susy;
-    _m_2           = _m_susy;
-    _m_3           = _m_susy;
-
-    _m_sl_1        = _m_susy;
-    _m_sl_2        = _m_susy;
-    _m_sl_3        = _m_susy;
-
-    _m_se_1        = _m_susy;
-    _m_se_2        = _m_susy;
-    _m_se_3        = _m_susy;
-    
-    _m_sq_1        = _m_susy;
-    _m_sq_2        = _m_susy;
-    _m_sq_3        = _m_susy;
-
-    _m_su_1        = _m_susy;
-    _m_su_2        = _m_susy;
-    _m_su_3        = _m_susy;
-
-    _m_sd_1        = _m_susy;
-    _m_sd_2        = _m_susy;
-    _m_sd_3        = _m_susy;
-
-    _mass_A0       = _m_susy;
-    _mass_Hp       = _m_susy;
-
-    _Re_mu         = _m_susy*_model->GetCollectionOfParameters().At("signMu")->GetValue();
-    _Im_mu         = 0;
-    _mu            = std::complex<double>(_Re_mu, _Im_mu);
-
-    _tanBeta       = _model->GetCollectionOfParameters().At("TanBeta")->GetValue();
-
-    double Xt =  _model->GetCollectionOfParameters().At("relXt")->GetValue()*_m_susy;
-
-    _Re_A_t        = Xt+_Re_mu/_tanBeta;
-    _Im_A_t        = 0;
-    _A_t           = std::complex<double>(_Re_A_t, _Im_A_t);   
-
-    _Re_A_b        = _Re_A_t;
-    _Im_A_b        = 0;
-    _A_b           = std::complex<double>(_Re_A_b, _Im_A_b);   
-
-    _Re_A_c        = _Re_A_t;           
-    _Im_A_c        = 0;
-    _A_c           = std::complex<double>(_Re_A_c, _Im_A_c);   
-
-    _Re_A_s        = _Re_A_t;
-    _Im_A_s        = 0;
-    _A_s           = std::complex<double>(_Re_A_s, _Im_A_s);   
-
-    _Re_A_d        = _Re_A_t;
-    _Im_A_d        = 0;
-    _A_d           = std::complex<double>(_Re_A_d, _Im_A_d);   
-
-    _Re_A_u        = _Re_A_t;
-    _Im_A_u        = 0;
-    _A_u           = std::complex<double>(_Re_A_u, _Im_A_u);   
-
-    _Re_A_tau      = _Re_A_t;
-    _Im_A_tau      = 0;
-    _A_tau         = std::complex<double>(_Re_A_tau, _Im_A_tau);   
-
-    _Re_A_mu       = _Re_A_t;
-    _Im_A_mu       = 0;
-    _A_mu          = std::complex<double>(_Re_A_mu, _Im_A_mu);   
-
-    _Re_A_e        = _Re_A_t;
-    _Im_A_e        = 0;
-    _A_e           = std::complex<double>(_Re_A_e, _Im_A_e);   
-
-    _Q_tau         = 0;
-    _Q_t           = 0;
-    _Q_b           = 0;
-
-    //    _scalefactor   = 1;
-    //     double mstop1 = TMath::Sqrt( TMath::Power(_m_susy, 2) + TMath::Power(_mass_t, 2) - TMath::Abs( Xt * _mass_t ) );
-    //     double mstop2 = TMath::Sqrt( TMath::Power(_m_susy, 2) + TMath::Power(_mass_t, 2) + TMath::Abs( Xt * _mass_t ) );
-    //    _scalefactor  = TMath::Sqrt( mstop1 * mstop2 ) / _mass_t;
-
-    _scalefactor = _m_susy / _mass_t;
-    
-    FHSetPara( &_error, _scalefactor,
-	       _mass_t,
-	       _tanBeta,
-	       _mass_A0, _mass_Hp,
-	       _m_sl_3, _m_se_3, _m_sq_3, _m_su_3, _m_sd_3,
-	       _m_sl_2, _m_se_2, _m_sq_2, _m_su_2, _m_sd_2,
-	       _m_sl_1, _m_se_1, _m_sq_1, _m_su_1, _m_sd_1,
-	       _mu,
-	       _A_tau, _A_t, _A_b, _A_mu, _A_c, _A_s, _A_e, _A_u, _A_d,
-	       _m_1, _m_2, _m_3,
-	       _Q_tau, _Q_t, _Q_b );
-
-    _derror = _error;
-
-}
-
-void Fittino::FeynHiggsModelCalculator::SetSMParameters(){
-
-    _alphaem_inv   = -1;
-    _alphas_mass_Z = -1;
-    _GF            = -1;
-    
-    _mass_e        = -1;
-    _mass_mu       = -1;
-    _mass_tau      = -1;
-
-    _mass_u_2GeV   = -1;
-    _mass_d_2GeV   = -1;
-    _mass_s_2GeV   = -1;
-    _mass_c_mass_c = -1;
-    _mass_b_mass_b = -1;
-
-    _mass_W         = -1;
-    _mass_Z         = -1;
-
-    _CKM_lambda    = -1;
-    _CKM_A         = -1;
-    _CKM_rhobar    = -1; 
-    _CKM_etabar    = -1; 
-
-    FHSetSMPara( &_error,
-		 _alphaem_inv,
-		 _alphas_mass_Z,
-		 _GF,
-		 _mass_e,
-		 _mass_u_2GeV,
-		 _mass_d_2GeV,
-		 _mass_mu,
-		 _mass_c_mass_c,
-		 _mass_s_2GeV,
-		 _mass_tau,
-		 _mass_b_mass_b,
-		 _mass_W,
-		 _mass_Z,
-		 _CKM_lambda,
-		 _CKM_A,
-		 _CKM_rhobar,  
-		 _CKM_etabar ); 
-
-    _derror = _error;
-    
-}
