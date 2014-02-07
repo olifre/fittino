8126f2b269be3b5231cb3ead23f3f473bff0255a
Mathias Uhlenbrock <uhlenbrock@physik.uni-bonn.de>
2014-02-07 22:17:45 +0000

    Removed exceptions here.

--- sources/kernel/src/Fittino.cpp
+++ sources/kernel/src/Fittino.cpp
@@ -10,13 +10,13 @@
 *                                                                              *
 * Authors     Philip  Bechtle     <philip.bechtle@desy.de>                     *
 *             Klaus   Desch       <desch@physik.uni-bonn.de>                   *
-*	      Mathias Uhlenbrock  <uhlenbrock@physik.uni-bonn.de>              *
-*	      Peter   Wienemann   <wienemann@physik.uni-bonn.de>               *
+*             Mathias Uhlenbrock  <uhlenbrock@physik.uni-bonn.de>              *
+*             Peter   Wienemann   <wienemann@physik.uni-bonn.de>               *
 *                                                                              *
 * Licence     This program is free software; you can redistribute it and/or    *
 *             modify it under the terms of the GNU General Public License as   *
-*	      published by the Free Software Foundation; either version 3 of   *
-*	      the License, or (at your option) any later version.              *
+*             published by the Free Software Foundation; either version 3 of   *
+*             the License, or (at your option) any later version.              *
 *                                                                              *
 *******************************************************************************/
 
@@ -31,54 +31,15 @@
  *  The central object is the Fittino::Controller.
  */
 
-//#include <iostream>
-
 #include "Controller.h"
-//#include "InputFileException.h"
-//#include "SLHAFileException.h"
-//#include "ModelCalculatorException.h"
-//#include "OptimizerException.h"
 
 int main( int argc, char** argv ) {
-
-    /*!  
-     *  \todo Short-term: Discuss usage of exceptions here.
-     */
-
-    //try {
 
     Fittino::Controller* controller = Fittino::Controller::GetInstance();
 
     controller->InitializeFittino( argc, argv );
     controller->ExecuteFittino();
     controller->TerminateFittino();
-
-    //}
-    //catch ( Fittino::InputFileException& inputFileException ) {
-
-    //    std::cout << inputFileException.what() << std::endl;
-
-    //}
-    //catch ( Fittino::SLHAFileException& slhaFileException ) {
-
-    //    std::cout << lesHouchesFileException.what() << std::endl;
-
-    //}
-    //catch ( Fittino::ModelCalculatorException& modelCalculatorException ) {
-
-    //    std::cout << modelCalculatorException.what() << std::endl;
-
-    //}
-    //catch ( Fittino::OptimizerException& optimizerException ) {
-
-    //    std::cout << optimizerException.what() << std::endl;
-
-    //}
-    //catch (...) {
-
-    //    std::cout << "Exception caught by default handler" << std::endl;
-
-    //}
 
     return 0;
 
