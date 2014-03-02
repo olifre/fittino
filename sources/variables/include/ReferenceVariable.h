5de61665061ffc22c64ddb6eae63a5854ea750a5
Mathias Uhlenbrock <uhlenbrock@physik.uni-bonn.de>
2014-03-02 18:34:28 +0000

    Enabled svn keyword expansion.

--- sources/variables/include/ReferenceVariable.h
+++ sources/variables/include/ReferenceVariable.h
@@ -8,7 +8,7 @@
 *                                                                              *
 * Description Variable which value is a reference                              *
 *                                                                              *
-* Authors     Bjoern Sarrazin <sarrazin@physik.uni-bonn.de>                    *
+* Authors     Bjoern Sarrazin  <sarrazin@physik.uni-bonn.de>                   *
 *                                                                              *
 * Licence     This program is free software; you can redistribute it and/or    *
 *             modify it under the terms of the GNU General Public License as   *
@@ -34,8 +34,8 @@ namespace Fittino {
    *  \ingroup variables
    *  \brief Variable which value is a reference.
    */
-  template< class T >
-  class ReferenceVariable : public VariableBase< T > {
+  template<class T>
+  class ReferenceVariable : public VariableBase<T> {
 
     public:
       /*!
@@ -43,35 +43,37 @@ namespace Fittino {
        */
       ReferenceVariable( const std::string& name, const T& value );
       /*!
-       *  Standard destructor.
-       */
-      virtual ~ReferenceVariable();
-      /*!
        *  Returns the value of the variable.
        */
       const T& GetValue() const;
+
+    public:
+      /*!
+       *  Standard destructor.
+       */
+      virtual  ~ReferenceVariable();
 
     private:
       const T& _value;
 
   };
 
-  template< class T>
-  ReferenceVariable< T >::ReferenceVariable( const std::string& name, const T& value )
-    : VariableBase< T >( name ),
+  template<class T>
+  ReferenceVariable<T>::ReferenceVariable( const std::string& name, const T& value )
+    : VariableBase<T>( name ),
       _value ( value ) {
 
   }
 
-  template< class T >
-  ReferenceVariable< T >::~ReferenceVariable() {
+  template<class T>
+  const T& ReferenceVariable<T>::GetValue() const {
+
+    return _value;
 
   }
 
-  template< class T >
-  const T& ReferenceVariable< T >::GetValue() const {
-
-    return _value;
+  template<class T>
+  ReferenceVariable<T>::~ReferenceVariable() {
 
   }
 
