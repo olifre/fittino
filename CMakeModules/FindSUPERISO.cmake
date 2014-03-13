eb004fd1a951948aad9d0f4073368d1ffcddb1c9
Björn Sarrazin <sarrazin@physik.uni-bonn.de>
2014-03-13 14:05:01 +0000

    Clean up.

--- CMakeModules/FindSUPERISO.cmake
+++ CMakeModules/FindSUPERISO.cmake
@@ -10,12 +10,11 @@
 #             If successful, it adds LHAPDF to Fittino as a cmake module.      #
 #                                                                              #
 # Authors     Bjoern  Sarrazin    <sarrazin@physik.uni-bonn.de>                #
-#             Mathias Uhlenbrock  <uhlenbrock@physik.uni-bonn.de>              #    
 #                                                                              #
 # Licence     This program is free software; you can redistribute it and/or    #
 #             modify it under the terms of the GNU General Public License as   #
-#	      published by the Free Software Foundation; either version 3 of       #
-#	      the License, or (at your option) any later version.                  #
+#	      published by the Free Software Foundation; either version 3 of   #
+#	      the License, or (at your option) any later version.              #
 #                                                                              #
 ################################################################################
 
