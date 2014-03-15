61f7cd49248490ae40f37971143cf2d24198ef4d
Björn Sarrazin <sarrazin@physik.uni-bonn.de>
2014-03-15 18:49:37 +0000

    Resurrected SLHAPrediction.

---
+++ sources/models/include/SLHAPrediction.h
@@ -0,0 +1,91 @@
+/* $Id$ */
+
+/*******************************************************************************
+*                                                                              *
+* Project     Fittino - A SUSY Parameter Fitting Package                       *
+*                                                                              *
+* File        SLHAPrediction.h                                                 *
+*                                                                              *
+* Description Class for SLHA predicitions                                      *
+*                                                                              *
+* Authors     Sebastian Heer  <s6seheer@uni-bonn.de>                           *
+*                                                                              *
+* Licence     This program is free software; you can redistribute it and/or    *
+*             modify it under the terms of the GNU General Public License as   *
+*             published by the Free Software Foundation; either version 3 of   *
+*             the License, or (at your option) any later version.              *
+*                                                                              *
+*******************************************************************************/
+
+#ifndef FITTINO_SLHAPREDICTION_H
+#define FITTINO_SLHAPREDICTION_H
+
+#include "PredictionBase.h"
+
+/*!
+ *  \brief Fittino namespace.
+ */
+namespace Fittino {
+
+  class SLHACalculatorBase;
+
+  /*!
+   *  \ingroup models
+   *  \brief Class for SLHA observables.
+   */
+  class SLHAPrediction : public PredictionBase {
+
+    public:
+      /*!
+       *  Constructor taking name, plotName, unit, plotUnit, plotLowerBound, plotUpperBound,
+       *  blockName, id, columnIndex and a calculator.
+       */
+      SLHAPrediction( std::string         name,
+                      std::string         plotName,
+                      std::string         unit,
+                      std::string         plotUnit,
+                      double              plotLowerBound,
+                      double              plotUpperBound,
+                      SLHACalculatorBase* slhaCalculator,
+                      std::string         blockName,
+                      std::string         id,
+                      int                 columnIndex );
+      /*!
+       *  Alternative constructor taking name, plotName, unit, plotUnit, plotLowerBound,
+       *  plotUpperBound, blcokName, firstId, secondId, columnIndex and a claculator.
+       */
+      SLHAPrediction( std::string         name,
+                      std::string         plotName,
+                      std::string         unit,
+                      std::string         plotUnit,
+                      double              plotLowerBound,
+                      double              plotUpperBound,
+                      SLHACalculatorBase* slhaCalculator,
+                      std::string         blockName,
+                      std::string         firstId,
+                      std::string         secondId,
+                      int                 columnIndex );
+      /*!
+       *  Standard Constructor
+       */
+      SLHAPrediction( const boost::property_tree::ptree& ptree, SLHACalculatorBase* slhaCalculator );
+      /*!
+       *  Standard destructor.
+       */
+      ~SLHAPrediction();
+
+    public:
+      virtual void        Update();
+
+    private:
+      int                 _columnIndex;
+      std::string         _firstId;
+      std::string         _secondId;
+      std::string         _blockName;
+      SLHACalculatorBase* _slhaCalculator;
+
+  };
+
+}
+
+#endif // FITTINO_SLHAPREDICTION_H
---
+++ sources/models/src/SLHAPrediction.cpp
@@ -0,0 +1,104 @@
+/* $Id$ */
+
+/*******************************************************************************
+*                                                                              *
+* Project     Fittino - A SUSY Parameter Fitting Package                       *
+*                                                                              *
+* File        SLHAPrediction.cpp                                               *
+*                                                                              *
+* Description Class for SLHA predictions                                       *
+*                                                                              *
+* Authors     Sebastian Heer  <s6seheer@uni-bonn.de>                           *
+*                                                                              *
+* Licence     This program is free software; you can redistribute it and/or    *
+*             modify it under the terms of the GNU General Public License as   *
+*             published by the Free Software Foundation; either version 3 of   *
+*             the License, or (at your option) any later version.              *
+*                                                                              *
+*******************************************************************************/
+
+#include "SLHADataStorageBase.h"
+#include "SLHACalculatorBase.h"
+#include "SLHAPrediction.h"
+#include "boost/property_tree/ptree.hpp"
+
+Fittino::SLHAPrediction::SLHAPrediction( std::string         name,
+                                         std::string         plotName,
+                                         std::string         unit,
+                                         std::string         plotUnit,
+                                         double              plotLowerBound,
+                                         double              plotUpperBound,
+                                         SLHACalculatorBase* slhaCalculator,
+                                         std::string         blockName,
+                                         std::string         id,
+                                         int                 columnIndex )
+    : _columnIndex( columnIndex ),
+      _firstId( id ),
+      _blockName( blockName ),
+      _slhaCalculator( slhaCalculator ),
+      PredictionBase( name,
+                      plotName,
+                      unit,
+                      plotUnit,
+                      plotLowerBound,
+                      plotUpperBound ) {
+
+    _secondId = "";
+
+}
+
+Fittino::SLHAPrediction::SLHAPrediction( std::string         name,
+                                         std::string         plotName,
+                                         std::string         unit,
+                                         std::string         plotUnit,
+                                         double              plotLowerBound,
+                                         double              plotUpperBound,
+                                         SLHACalculatorBase* slhaCalculator,
+                                         std::string         blockName,
+                                         std::string         firstId,
+                                         std::string         secondId,
+                                         int                 columnIndex )
+    : _columnIndex( columnIndex ),
+      _firstId( firstId ),
+      _secondId( secondId ),
+      _blockName( blockName ),
+      _slhaCalculator( slhaCalculator ),
+      PredictionBase( name,
+                      plotName,
+                      unit,
+                      plotUnit,
+                      plotLowerBound,
+                      plotUpperBound ) {
+
+}
+
+Fittino::SLHAPrediction::SLHAPrediction( const boost::property_tree::ptree& ptree, SLHACalculatorBase* slhaCalculator )
+    : _columnIndex( ptree.get<int>( "ColumnIndex" ) ),
+      _firstId( ptree.get<std::string>( "FirstId" ) ),
+      _secondId( ptree.get<std::string>( "SecondId", "" ) ),
+      _blockName( ptree.get<std::string>( "BlockName" ) ),
+      _slhaCalculator( slhaCalculator ),
+      PredictionBase( ptree ) {
+
+}
+
+Fittino::SLHAPrediction::~SLHAPrediction() {
+
+}
+
+
+void Fittino::SLHAPrediction::Update() {
+
+    if ( _secondId == "" ) {
+
+        _value = _slhaCalculator->GetDataStorage()->GetEntry( _blockName, _firstId, _columnIndex );
+
+    }
+    else {
+
+        _value = _slhaCalculator->GetDataStorage()->GetEntry( _blockName, _firstId, _secondId, _columnIndex );
+
+    }
+
+}
+
