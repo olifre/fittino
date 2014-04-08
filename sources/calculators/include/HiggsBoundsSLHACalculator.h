4546e9ec9d8b107703f92edbb3325d2f1a7a0bdc
Matthias Hamer <mhamer@uni-goettingen.de>
2014-04-08 17:57:50 +0000

    add HiggsBoundsSLHACalculator

---
+++ sources/calculators/include/HiggsBoundsSLHACalculator.h
@@ -0,0 +1,55 @@
+/* $Id: HiggsBoundsSLHACalculator.h 2058 2014-04-07 20:03:16Z mhamer $ */
+
+/*******************************************************************************
+*                                                                              *
+* Project     Fittino - A SUSY Parameter Fitting Package                       *
+*                                                                              *
+* File        HiggsBoundsSLHACalculator.h                                          *
+*                                                                              *
+* Description Wrapper class for HiggsBounds                                    *
+*                                                                              *
+* Authors     Matthias Hamer  <mhamer@gwdg.de>                                 *
+*                                                                              *
+* Licence     This program is free software; you can redistribute it and/or    *
+*             modify it under the terms of the GNU General Public License as   *
+*             published by the Free Software Foundation; either version 3 of   *
+*             the License, or (at your option) any later version.              *
+*                                                                              *
+*******************************************************************************/
+
+#ifndef FITTINO_HIGGSBOUNDSSLHACALCULATOR_H
+#define FITTINO_HIGGSBOUNDSSLHACALCULATOR_H
+
+#include "HiggsBoundsCalculator.h"
+
+/*!
+ *  \brief Fittino namespace.
+ */
+namespace Fittino {
+
+  /*!
+   *  \ingroup calculators
+   *  \brief Wrapper class for HiggsBounds.
+   */
+  class HiggsBoundsSLHACalculator : public HiggsBoundsCalculator {
+
+    public:
+      /*!
+       *  Standard constructor.
+       */
+      HiggsBoundsSLHACalculator( const PhysicsModel* model, const boost::property_tree::ptree& ptree );
+      /*!
+       *  Standard destructor.
+       */
+      ~HiggsBoundsSLHACalculator();
+
+    private:
+      virtual void setupHiggsBounds();
+
+      /*! \endcond UML */
+
+  };
+
+}
+
+#endif // FITTINO_HIGGSBOUNDSSLHACALCULATOR_H
---
+++ sources/calculators/src/HiggsBoundsSLHACalculator.cpp
@@ -0,0 +1,48 @@
+/* $Id: HiggsBoundsSLHACalculator.cpp 2058 2014-04-07 20:03:16Z mhamer $ */
+
+/*******************************************************************************
+*                                                                              *
+* Project     Fittino - A SUSY Parameter Fitting Package                       *
+*                                                                              *
+* File        HiggsBoundsSLHACalculator.cpp                                        *
+*                                                                              *
+* Description Wrapper class for HiggsBounds                                    *
+*                                                                              *
+* Authors     Matthias Hamer  <mhamer@gwdg.de>                                 *
+*                                                                              *
+* Licence     This program is free software; you can redistribute it and/or    *
+*             modify it under the terms of the GNU General Public License as   *
+*             published by the Free Software Foundation; either version 3 of   *
+*             the License, or (at your option) any later version.              *
+*                                                                              *
+*******************************************************************************/
+
+#include <cmath>
+
+#include <boost/thread.hpp>
+
+#include "CHiggsSignals.h"
+#include "ConfigurationException.h"
+#include "HiggsBoundsSLHACalculator.h"
+#include "PhysicsModel.h"
+#include "SimplePrediction.h"
+#include "Observable.h"
+
+Fittino::HiggsBoundsSLHACalculator::HiggsBoundsSLHACalculator( const PhysicsModel* model, const boost::property_tree::ptree& ptree ) 
+    : HiggsBoundsCalculator( model, ptree ) {
+
+}
+
+Fittino::HiggsBoundsSLHACalculator::~HiggsBoundsSLHACalculator() {
+
+}
+
+void Fittino::HiggsBoundsSLHACalculator::setupHiggsBounds() {
+
+    std::string filename = "SPheno.spc";
+    int length = filename.length();
+    char charfilename[256];
+    sprintf( charfilename, "%s", filename.c_str() );
+    higgsbounds_input_slha_(charfilename, &length );
+
+}
