db53770a53c808e396a12c60006d56134607f6b8
Matthias Hamer <mhamer@uni-goettingen.de>
2014-05-02 19:07:56 +0000

    added class CutBase

---
+++ sources/kernel/include/CutBase.h
@@ -0,0 +1,55 @@
+/*******************************************************************************
+*                                                                              *
+* Project     Fittino - A SUSY Parameter Fitting Package                       *
+*                                                                              *
+* File        CutBase.h                                                        *
+*                                                                              *
+* Description Base class for cuts                                              *
+*                                                                              *
+* Authors     Matthias Hamer      <mhamer@cbpf.br>                             *
+*                                                                              *
+* Licence     This program is free software; you can redistribute it and/or    *
+*             modify it under the terms of the GNU General Public License as   *
+*             published by the Free Software Foundation; either version 3 of   *
+*             the License, or (at your option) any later version.              *
+*                                                                              *
+*******************************************************************************/
+
+#ifndef FITTINO_CUTBASE_H
+#define FITTINO_CUTBASE_H
+
+#include <string>
+
+/*!
+ *  \brief Fittino namespace.
+ */
+namespace Fittino {
+
+  /*!
+   *  \ingroup kernel
+   *  \brief Base class for data storages.
+   */
+  class CutBase {
+
+    public:
+      /*!
+       *  Standard constructor.
+       */
+      CutBase();
+
+    public:
+      /*!
+       *  Standard destructor.
+       */
+      virtual      ~CutBase();
+
+      /*!
+       * Determines whether a cut is passed or not.
+       */
+      virtual bool        IsPassed() = 0;
+
+  };
+
+}
+
+#endif // FITTINO_CUTBASE_H
---
+++ sources/kernel/src/CutBase.cpp
@@ -0,0 +1,26 @@
+/*******************************************************************************
+*                                                                              *
+* Project     Fittino - A SUSY Parameter Fitting Package                       *
+*                                                                              *
+* File        CutBase.cpp                                              *
+*                                                                              *
+* Description Base class for cuts                                     *
+*                                                                              *
+* Authors     Matthias Hamer      <mhamer@cbpf.br>                             *
+*                                                                              *
+* Licence     This program is free software; you can redistribute it and/or    *
+*             modify it under the terms of the GNU General Public License as   *
+*             published by the Free Software Foundation; either version 3 of   *
+*             the License, or (at your option) any later version.              *
+*                                                                              *
+*******************************************************************************/
+
+#include "CutBase.h"
+
+Fittino::CutBase::CutBase() {
+
+}
+
+Fittino::CutBase::~CutBase() {
+
+}
