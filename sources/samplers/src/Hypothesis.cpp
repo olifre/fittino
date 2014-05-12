28b89eb32394d95f63b337abea14dc88389903b7
Pia Kullik <kullik@physik.uni-bonn.de>
2014-05-12 15:29:32 +0000

    Added classes for evolutionary algorithm sampler.

---
+++ sources/samplers/src/Hypothesis.cpp
@@ -0,0 +1,37 @@
+#include "Hypothesis.h"
+//#include "ModelParameterBase.h"
+#include <vector>
+#include <cmath>
+using namespace Fittino;
+
+//Constructor
+Fittino::Hypothesis::Hypothesis()
+{
+    this->_fitness = -INFINITY;
+}
+
+//Destructor
+Fittino::Hypothesis::~Hypothesis()
+{
+    //dtor
+}
+
+//Parameter vector getter/setter
+std::vector<ModelParameterBase> Fittino::Hypothesis::getParameterVector(){
+    return this->_parameterVector;
+}
+
+void Fittino::Hypothesis::setParameterVector(int i, double value){
+    this->getParameterVector().at(i).SetValue(value);
+}
+
+//Fitness getter/setter
+double Fittino::Hypothesis::getFitness(){
+    return this->_fitness;
+}
+
+void Fittino::Hypothesis::setFitness(double value){
+    this->_fitness = value;
+}
+
+
