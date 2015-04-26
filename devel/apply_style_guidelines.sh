30d9cef76990dcceed48958dcda2c8c2f07b6b12
Mathias Uhlenbrock <uhlenbrock@physik.uni-bonn.de>
2015-04-26 14:30:01 +0000

    Added new make target "stylish".
    By typing
    
      make stylish
    
    the script devel/apply_style_guidelines.sh runs astyle on all source files.

---
+++ devel/apply_style_guidelines.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+apply_style_guidelines() {
+
+    MODULENAME=$1
+
+    echo -e "\nApplying style guidelines to module \"${MODULENAME}\"...\n"
+
+    # Check if include directory exists.
+
+    if [ -d "../sources/${MODULENAME}/include" ]; then
+
+        # Loop over all files.
+
+        for file in ../sources/${MODULENAME}/include/*; do
+
+            # If there are files in the directory, apply style guidelines via astyle.
+
+            if [ -e "$file" ]; then
+
+                astyle --options=../devel/astyle-header-options $file
+
+            fi
+
+        done
+
+        # Remove .orig files if there are any.
+
+        for file in ../sources/${MODULENAME}/include/*.orig; do
+
+            if [ -e "$file" ]; then
+
+                rm $file
+
+            fi
+
+        done
+
+    fi
+
+    # Do the same with source files.
+
+    if [ -d "../sources/${MODULENAME}/src" ]; then
+
+        for file in ../sources/${MODULENAME}/src/*; do
+
+            if [ -e "$file" ]; then
+
+                astyle --options=../devel/astyle-options $file
+
+            fi
+
+        done
+
+        for file in ../sources/${MODULENAME}/src/*.orig; do
+
+            if [ -e "$file" ]; then
+
+                rm $file
+
+            fi
+
+        done
+
+    fi
+
+}
+
+#apply_style_guidelines calculators 
+apply_style_guidelines datastorages
+apply_style_guidelines exceptions
+apply_style_guidelines histogrammakers
+apply_style_guidelines kernel 
+#apply_style_guidelines models 
+apply_style_guidelines optimizers
+apply_style_guidelines plotters
+apply_style_guidelines samplers
+apply_style_guidelines tools
+apply_style_guidelines variables 
+
+echo ""
