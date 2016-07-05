f65c153c0ddf29c24a810b9ac625a58a6f691bc9
Björn Sarrazin <sarrazin@physik.uni-bonn.de>
2016-07-05 17:06:45 +0000

    Added 2 std namespaces.

--- sources/calculators/src/GCECalculator.cpp
+++ sources/calculators/src/GCECalculator.cpp
@@ -63,7 +63,7 @@ void Fittino::GCECalculator::CalculatePredictions() {
   
     redirector.Start();
     
-    ofstream inputFile( "GCEInput.txt", std::ofstream::out );
+    std::ofstream inputFile( "GCEInput.txt", std::ofstream::out );
     inputFile << "sigmav  " << _model->GetCollectionOfQuantities().At("MicrOMEGAs_vSigma")->GetValue() << std::endl;
     inputFile << "mDM "    << _model->GetCollectionOfQuantities().At("SPheno_Mass_~chi10")->GetValue() << std::endl;
     inputFile << "con_gg " << _model->GetCollectionOfQuantities().At("MicrOMEGAs_vSigma_Contribution_GG")->GetValue() << std::endl;
@@ -100,7 +100,7 @@ void Fittino::GCECalculator::CalculatePredictions() {
     redirector.Stop();
     
     inputFile.close();
-    ifstream outputFile( "GCEOutput.txt", std::ifstream::in );
+    std::ifstream outputFile( "GCEOutput.txt", std::ifstream::in );
     std::string key, value;
     while( outputFile.good() ) {
         outputFile >> key >> std::ws >> value;
