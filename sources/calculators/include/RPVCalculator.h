3531899ec865358c66c98b43bb1c5cb7a2c05003
Björn Sarrazin <sarrazin@physik.uni-bonn.de>
2017-11-09 11:58:34 +0000

    RPVCalculator now uses full CKM Matrix in Wolfenstein parametrization.

--- sources/calculators/include/RPVCalculator.h
+++ sources/calculators/include/RPVCalculator.h
@@ -2,6 +2,7 @@
 #define FITTINO_RPVCALCULATOR_H
 
 #include "CalculatorBase.h"
+#include <complex>
 
 namespace Fittino {
     
@@ -11,18 +12,19 @@ namespace Fittino {
         
         public:
         
+        
         RPVCalculator( const ModelBase* model, const boost::property_tree::ptree& ptree);
         ~RPVCalculator();
         private:
-        double          Vud;
-        double          Vus;
-        double          Vub;
-        double          Vcd;
-        double          Vcs;
-        double          Vcb;
-        double          Vtd;
-        double          Vts;
-        double          Vtb;
+        std::complex<double>          Vud;
+        std::complex<double>          Vus;
+        std::complex<double>          Vub;
+        std::complex<double>          Vcd;
+        std::complex<double>          Vcs;
+        std::complex<double>          Vcb;
+        std::complex<double>          Vtd;
+        std::complex<double>          Vts;
+        std::complex<double>          Vtb;
         double          s_Wsq;
         double          alpha_em;
         double          X_t;
@@ -44,10 +46,11 @@ namespace Fittino {
         virtual void CalculatePredictions();
         virtual void Initialize();
         
+    private:
         
-        
-        
-        
+        void InitializeCKMMatrix();
+
+    
     };
     
 }
--- sources/calculators/src/RPVCalculator.cpp
+++ sources/calculators/src/RPVCalculator.cpp
@@ -24,16 +24,11 @@ Fittino::RPVCalculator::RPVCalculator(const ModelBase *model, const boost::prope
     AddOutput("NormSM_BR_B_pi_nu_nu",B_pi_relation);
     AddOutput("Ratio_gZtautau_gZll",Z_coupling_relation);
     AddOutput("Ratio_gWtaunu_gWlnu",W_coupling_relation);
+    
     X_t = 1.469;
-    Vud = 0.97434;
-    Vus = 0.22506;
-    Vub = 0.00357;
-    Vcd = 0.22492;
-    Vcs = 0.97351;
-    Vcb = 0.0411;
-    Vtd = 0.00875;
-    Vts = 0.0403;
-    Vtb = 0.99915;
+    
+    InitializeCKMMatrix();
+    
     //at W_Z & m_t direct measurement
     s_Wsq = 0.22336;
     alpha_em = 1./128.;
@@ -61,18 +56,14 @@ void Fittino::RPVCalculator::CalculatePredictions() {
     const double& vev = GetInput( "Vev" );
 
     // 0.25 instead of 0.5 in literature
-    R_D_Relation = 1. + 0.25*pow(vev/mbR,2)*(pow(lambda_333,2) + lambda_333*lambda_323*Vcs/Vcb + lambda_333*lambda_313*Vcd/Vcb);
-    R_D_Relation = pow(R_D_Relation,2);
+    R_D_Relation = std::norm( 1. + 0.25 * pow( vev / mbR, 2 ) * ( pow( lambda_333, 2 ) + lambda_333 * lambda_323 * Vcs / Vcb + lambda_333 * lambda_313 * Vcd / Vcb ) );
     R_Dstar_Relation = R_D_Relation;
     
-    B_tau_nu_relation = 1. + 0.5*pow(vev/mbR,2)*(pow(lambda_333,2) + lambda_333*lambda_323*Vus/Vub + lambda_333*lambda_313*Vud/Vub);
-    B_tau_nu_relation = pow(B_tau_nu_relation,2);
+    B_tau_nu_relation = std::norm( 1. + 0.5 * pow( vev / mbR, 2 ) * ( pow( lambda_333, 2 ) + lambda_333 * lambda_323 * Vus / Vub + lambda_333 * lambda_313 * Vud / Vub ) );
     
-    B_K_relation = 1. + pow(vev/mbR,2)*M_PI*s_Wsq*lambda_333*lambda_323/(alpha_em*Vtb*Vts*X_t);
-    B_K_relation = 2./3 + 1./3.*pow(B_K_relation,2);
+    B_K_relation  = 2./3. + 1./3. * std::norm( 1. + pow( vev / mbR, 2 ) * M_PI * s_Wsq * lambda_333 * lambda_323 / ( alpha_em * Vtb * std::conj( Vts ) * X_t ) );
     
-    B_pi_relation = 1. + pow(vev/mbR,2)*M_PI*s_Wsq*lambda_333*lambda_313/(alpha_em*Vtb*Vts*X_t);
-    B_pi_relation = 2./3. + 1./3.*pow(B_pi_relation,2);
+    B_pi_relation = 2./3. + 1./3. * std::norm( 1. + pow( vev / mbR, 2 ) * M_PI * s_Wsq * lambda_333 * lambda_313 / ( alpha_em * Vtb * std::conj( Vtd ) * X_t ) );
     
     f_Z = 1./(pow(m_t/mbR,2)-1.) - log10(pow(m_t/mbR,2))/pow((pow(m_t/mbR,2)-1.),2);
     Z_coupling_relation = 1. - 3.*pow(lambda_333/M_PI,2)*pow(m_t/mbR,2)*f_Z/(16.*(1.-2.*s_Wsq));
@@ -87,5 +78,67 @@ void Fittino::RPVCalculator::Initialize() {
     
 }
 
+void Fittino::RPVCalculator::InitializeCKMMatrix() {
+    
+    // CKM Matrix according to http://pdg.lbl.gov/2016/reviews/rpp2016-rev-ckm-matrix.pdf
+    
+    // Start Equation 12.26
+    
+    double lambda = 0.22506;
+    double A = 0.811;
+    double rhobar = 0.124;
+    double etabar = 0.356;
+    
+    // End Equation 12.26
+    
+    // Start Equation 12.4
+    
+    double s12 = lambda;
+    double s23 = A * std::pow( lambda, 2 );
+    
+    const std::complex<double> i(0, 1);
+    std::complex<double> rhoeta = rhobar + i * etabar;
+    
+    double A2lambda4 = std::pow( A, 2 ) * std::pow( lambda, 4 );
+    
+    std::complex<double> s13eidelta =  A * std::pow( lambda, 3 ) * rhoeta * std::sqrt( 1. - A2lambda4  );
+    s13eidelta /= std::sqrt( 1. - std::pow( lambda, 2 ) ) * ( 1. - A2lambda4 * rhoeta );
+    
+    // End Equation 12.4
+    
+    double c12 = std::sqrt( 1. - std::norm( s12        ) );
+    double c13 = std::sqrt( 1. - std::norm( s13eidelta ) );
+    double c23 = std::sqrt( 1. - std::norm( s23        ) );
+    
+    // Start Equation 12.3
+    
+    Vud = c12 * c13;
+    Vus = s12 * c13;
+    Vub = std::conj( s13eidelta );
+    
+    Vcd = -s12 * c23 - c12 * s23 * s13eidelta;
+    Vcs =  c12 * c23 - s12 * s23 * s13eidelta;
+    Vcb =  s23 * c13;
+    
+    Vtd =  s12 * s23 - c12 * c23 * s13eidelta;
+    Vts = -c12 * s23 - s12 * c23 * s13eidelta;
+    Vtb =  c23 * c13;
+    
+    // End Equation 12.3
+    
+    auto& messenger = Messenger::GetInstance();
+    messenger << Messenger::DEBUG << "CKM Matrix initialized to:" << Messenger::Endl;
+    messenger << Messenger::DEBUG << Vud << "  " << Vus << "  " << Vub <<  Messenger::Endl;
+    messenger << Messenger::DEBUG << Vcd << "  " << Vcs << "  " << Vcb <<  Messenger::Endl;
+    messenger << Messenger::DEBUG << Vtd << "  " << Vts << "  " << Vtb <<  Messenger::Endl;
+    messenger << Messenger::DEBUG << Messenger::Endl;
+    
+    messenger << Messenger::DEBUG << "Absolute Values are:" << Messenger::Endl;
+    messenger << Messenger::DEBUG << std::abs( Vud ) << "  " << std::abs( Vus ) << "  " << std::abs( Vub ) <<  Messenger::Endl;
+    messenger << Messenger::DEBUG << std::abs( Vcd ) << "  " << std::abs( Vcs ) << "  " << std::abs( Vcb ) <<  Messenger::Endl;
+    messenger << Messenger::DEBUG << std::abs( Vtd ) << "  " << std::abs( Vts ) << "  " << std::abs( Vtb ) <<  Messenger::Endl;
+    messenger << Messenger::DEBUG << Messenger::Endl;
+    
+}
 
 
