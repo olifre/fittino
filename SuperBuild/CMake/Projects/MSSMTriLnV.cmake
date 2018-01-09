7bb02debdfa4a37e2b19287693465fd6808af03d
Björn Sarrazin <sarrazin@physik.uni-bonn.de>
2018-01-09 11:05:40 +0000

    Cleanup the MSSMTriLnV installation

--- SuperBuild/CMake/Projects/MSSMTriLnV.cmake
+++ SuperBuild/CMake/Projects/MSSMTriLnV.cmake
@@ -2,9 +2,6 @@ set( MSSMTriLnV MSSMTriLnV )
 
 enable_language( Fortran )
 
-set( MSSMTriLnV_URL "~/Downloads/MSSMTriLnV.tgz" CACHE FILEPATH "" )
-set( MSSMTriLnV_MD5 eb54d22459a6f9482fd4e959c69ff00e )
-
 # see https://sarah.hepforge.org/sarah_in_a_nutshell.pdf
 
 externalproject_add(
@@ -12,9 +9,9 @@ externalproject_add(
     ${MSSMTriLnV}
     DEPENDS ${SPheno}
     URL ${CMAKE_CURRENT_SOURCE_DIR}/MSSMTriLnV
-    #URL_MD5 ${MSSMTriLnV_MD5}
+    SOURCE_DIR ${SPheno_ROOT_DIR}/MSSMTriLnV
     PATCH_COMMAND ${CMAKE_COMMAND} -DMAKEFILE=Makefile -P ${CMAKE_CURRENT_SOURCE_DIR}/CMake/Scripts/patch_SPheno.cmake
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR> ${SPheno_ROOT_DIR}/MSSMTriLnV
+    CONFIGURE_COMMAND ""
     BINARY_DIR ${SPheno_ROOT_DIR}
     BUILD_COMMAND $(MAKE) Model=MSSMTriLnV F90=${CMAKE_Fortran_COMPILER}
     INSTALL_COMMAND ""
