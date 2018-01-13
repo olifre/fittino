fa4250d917786bf0be8f4c5d0aadedacefec8ca3
Björn Sarrazin <sarrazin@physik.uni-bonn.de>
2018-01-13 15:20:12 +0000

    Fix typo
    As a result of this typo SModelS was falsely reported as not been found without any further effect.

--- CMakeModules/FindSModelS.cmake
+++ CMakeModules/FindSModelS.cmake
@@ -6,7 +6,7 @@ get_filename_component( bindir ${SModelS_Tools_EXECUTABLE} DIRECTORY )
 
 find_path( SModelS_Database_DIR database.bib HINTS ${SModelS_ROOT_DIR}/smodels-database ${bindir}/../smodels-database )
 
-find_package_handle_standard_args( SModelS FOUND_VAR SModelS_FOUND REQUIRED_VARS SModelSTools_EXECUTABLE SModelS_Database_DIR )
+find_package_handle_standard_args( SModelS FOUND_VAR SModelS_FOUND REQUIRED_VARS SModelS_Tools_EXECUTABLE SModelS_Database_DIR )
 
 mark_as_advanced(
 
