48998352e4edccd0041badbb8fa2fd706401f555
Björn Sarrazin <sarrazin@physik.uni-bonn.de>
2018-01-28 12:54:49 +0000

    Add initial cache file for minimal fittino installation without Python

---
+++ SuperBuild/CMake/InitialCacheFiles/MinimalFittino.cmake
@@ -0,0 +1,3 @@
+set( INSTALL_Fittino                         ON CACHE BOOL "" FORCE )
+set( CMAKE_DISABLE_FIND_PACKAGE_PythonInterp ON CACHE BOOL "" FORCE )
+set( CMAKE_DISABLE_FIND_PACKAGE_PythonLibs   ON CACHE BOOL "" FORCE )
