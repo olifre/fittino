59ad424f04e5e68b85f6471b6e5f974e2401fb87
Björn Sarrazin <sarrazin@physik.uni-bonn.de>
2018-01-28 12:53:23 +0000

    Write Mac compiler settings in separate initial cache file

---
+++ SuperBuild/CMake/InitialCacheFiles/CompilerMacOS.cmake
@@ -0,0 +1,3 @@
+set( CMAKE_CXX_COMPILER     /usr/bin/clang++              CACHE FILEPATH "" FORCE )
+set( CMAKE_C_COMPILER       /usr/bin/clang                CACHE FILEPATH "" FORCE )
+set( CMAKE_Fortran_COMPILER /opt/local/bin/gfortran-mp-7  CACHE FILEPATH "" FORCE )
--- SuperBuild/CMake/InitialCacheFiles/MissingTopologies.cmake
+++ SuperBuild/CMake/InitialCacheFiles/MissingTopologies.cmake
@@ -1,11 +1,6 @@
 set( INSTALL_Python2 ON CACHE BOOL "" FORCE )
 set( INSTALL_SModelS ON CACHE BOOL "" FORCE )
-set( INSTALL_SCYNet ON CACHE BOOL "" FORCE )
-set( INSTALL_SLHAea ON CACHE BOOL "" FORCE )
-set( INSTALL_SPheno ON CACHE BOOL "" FORCE )
+set( INSTALL_SCYNet  ON CACHE BOOL "" FORCE )
+set( INSTALL_SLHAea  ON CACHE BOOL "" FORCE )
+set( INSTALL_SPheno  ON CACHE BOOL "" FORCE )
 set( INSTALL_Fittino ON CACHE BOOL "" FORCE )
-
-# possible compiler settings for mac
-# set( CMAKE_Fortran_COMPILER /opt/local/bin/gfortran-mp-7  CACHE PATH "" )
-# set( CMAKE_CXX_COMPILER /usr/bin/clang++ CACHE PATH "" )
-#set( CMAKE_C_COMPILER /usr/bin/clang CACHE PATH "" )
