3651c6f27d8436e0ba9bb7b0ae22505e5aaba976
Björn Sarrazin <sarrazin@physik.uni-bonn.de>
2018-04-16 16:08:49 +0000

    Make sure that the USE_SPhenoMSSMTriLnV_LOOPS option is given to the patch script

--- SuperBuild/CMake/Projects/MSSMTriLnV.cmake
+++ SuperBuild/CMake/Projects/MSSMTriLnV.cmake
@@ -13,7 +13,7 @@ externalproject_add(
     URL ${MSSMTriLnV_url}
     SOURCE_DIR ${SPheno_ROOT_DIR}/MSSMTriLnV
     PATCH_COMMAND ${CMAKE_COMMAND} -DMAKEFILE=Makefile -P ${CMAKE_CURRENT_SOURCE_DIR}/CMake/Scripts/patch_SPheno.cmake
-    COMMAND ${CMAKE_COMMAND} -DSRCFILE=SPhenoMSSMTriLnV.f90 -P ${CMAKE_CURRENT_SOURCE_DIR}/CMake/Scripts/patch_SPhenoMSSMTriLnV.cmake
+    COMMAND ${CMAKE_COMMAND} -DSRCFILE=SPhenoMSSMTriLnV.f90 -DUSE_SPhenoMSSMTriLnV_LOOPS=${USE_SPhenoMSSMTriLnV_LOOPS} -P ${CMAKE_CURRENT_SOURCE_DIR}/CMake/Scripts/patch_SPhenoMSSMTriLnV.cmake
     CONFIGURE_COMMAND ""
     BINARY_DIR ${SPheno_ROOT_DIR}
     BUILD_COMMAND $(MAKE) Model=MSSMTriLnV F90=${CMAKE_Fortran_COMPILER}
@@ -21,9 +21,8 @@ externalproject_add(
 
 )
 
-ExternalProject_Add_StepDependencies( ${MSSMTriLnV} download $ ${MSSMTriLnV_url} )
-
-ExternalProject_Add_StepDependencies( ${MSSMTriLnV} patch ${CMAKE_CURRENT_SOURCE_DIR}/CMake/Scripts/patch_SPheno.cmake ${CMAKE_CURRENT_SOURCE_DIR}/CMake/Scripts/patch_SPhenoMSSMTriLnV.cmake )
+# these are partially actually patch dependencies but in order to start from scratch with the patch they are declared as download dependencies
+ExternalProject_Add_StepDependencies( ${MSSMTriLnV} download ${CMAKE_CURRENT_SOURCE_DIR}/CMake/Scripts/patch_SPheno.cmake ${CMAKE_CURRENT_SOURCE_DIR}/CMake/Scripts/patch_SPhenoMSSMTriLnV.cmake ${MSSMTriLnV_url} )
 
 list( APPEND CACHE_ARGS -DSPheno_MSSMTriLnV_EXECUTABLE:FILEPATH=${SPheno_ROOT_DIR}/bin/SPhenoMSSMTriLnV )
 
