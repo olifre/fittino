3f2fcf714bc243824c9f323e0a7972a579d16d8d
Björn Sarrazin <sarrazin@physik.uni-bonn.de>
2018-04-16 13:51:41 +0000

    Set CXX_STANDARD to 17 in the example compiler configuration for macOS.

--- SuperBuild/CMake/InitialCacheFiles/CompilerMacOS.cmake
+++ SuperBuild/CMake/InitialCacheFiles/CompilerMacOS.cmake
@@ -1,3 +1,4 @@
 set( CMAKE_CXX_COMPILER     /usr/bin/clang++              CACHE FILEPATH "" FORCE )
 set( CMAKE_C_COMPILER       /usr/bin/clang                CACHE FILEPATH "" FORCE )
 set( CMAKE_Fortran_COMPILER /opt/local/bin/gfortran-mp-7  CACHE FILEPATH "" FORCE )
+set( CMAKE_CXX_STANDARD 17  CACHE STRING "" FORCE )
