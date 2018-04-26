2a199a86c714df781b8c52c720470fc259edfcd0
Björn Sarrazin <sarrazin@physik.uni-bonn.de>
2018-04-26 10:13:18 +0000

    Adapt patch_SPhenoMSSMTriLnV to changed default epsTree parameter

--- SuperBuild/CMake/Scripts/patch_SPhenoMSSMTriLnV.cmake
+++ SuperBuild/CMake/Scripts/patch_SPhenoMSSMTriLnV.cmake
@@ -1,10 +1,15 @@
+# todo: Use regex
 
-if( NOT USE_SPhenoMSSMTriLnV_LOOPS )
+FILE( READ ${SRCFILE} fileContent )
 
-    FILE( READ ${SRCFILE} fileContent )
+if( USE_SPhenoMSSMTriLnV_LOOPS )
 
-    STRING( REPLACE "epsTree=1.0E-1_dp" "epsTree=0.0E-1_dp" fileContent ${fileContent} )
+    STRING( REPLACE "epsTree=0.0E-1_dp" "epsTree=0.1E-1_dp" fileContent ${fileContent} )
 
-    FILE ( WRITE ${SRCFILE} ${fileContent} )
+else()
+
+    STRING( REPLACE "epsTree=0.0E-1_dp" "epsTree=0.0E-1_dp" fileContent ${fileContent} )
 
 endif()
+
+FILE ( WRITE ${SRCFILE} ${fileContent} )
