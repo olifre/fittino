e9abe9ae0c2c0184be8e7453ddf08dea2e90a134
Björn Sarrazin <sarrazin@physik.uni-bonn.de>
2018-05-06 15:29:31 +0000

    Allow to change USE_SPhenoMSSMTriLnV_LOOPS back after first patch step

--- SuperBuild/CMake/Scripts/patch_SPhenoMSSMTriLnV.cmake
+++ SuperBuild/CMake/Scripts/patch_SPhenoMSSMTriLnV.cmake
@@ -8,7 +8,7 @@ if( USE_SPhenoMSSMTriLnV_LOOPS )
 
 else()
 
-    STRING( REPLACE "epsTree=0.0E-1_dp" "epsTree=0.0E-1_dp" fileContent ${fileContent} )
+    STRING( REPLACE "epsTree=0.1E-1_dp" "epsTree=0.0E-1_dp" fileContent ${fileContent} )
 
 endif()
 
