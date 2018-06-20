89d8e0350969c3b8fad4eeb667f503f7f44ac84e
Björn Sarrazin <sarrazin@physik.uni-bonn.de>
2018-06-20 14:25:16 +0000

    Deactivate Title and StatBox in plotting

--- sources/plotters/src/Plotter.cpp
+++ sources/plotters/src/Plotter.cpp
@@ -1,5 +1,6 @@
 #include "Plotter.h"
 #include "Tool.h"
+#include "TROOT.h"
 #include <iostream>
 
 Fittino::Plotter::Plotter(const boost::property_tree::ptree& ptree )
@@ -14,6 +15,36 @@ Fittino::Plotter::Plotter(const boost::property_tree::ptree& ptree )
     }
         
     }
+    
+    _fittinoStyle = new TStyle( "FITTINO", "Fittino style" ) ;
+    _fittinoStyle->SetOptTitle( 0 );
+    _fittinoStyle->SetOptStat( 0 );
+    
+//    _fittinoStyle->SetTextFont( _textFont );
+//    _fittinoStyle->SetLabelFont( _textFont, "x" );
+//    _fittinoStyle->SetTitleFont( _textFont, "x" );
+//    _fittinoStyle->SetLabelFont( _textFont, "y" );
+//    _fittinoStyle->SetTitleFont( _textFont, "y" );
+//    _fittinoStyle->SetLabelFont( _textFont, "z" );
+//    _fittinoStyle->SetTitleFont( _textFont, "z" );
+//
+//    _fittinoStyle->SetTextSize( _textSize );
+//    _fittinoStyle->SetLabelSize( _textSize, "x" );
+//    _fittinoStyle->SetTitleSize( _textSize, "x" );
+//    _fittinoStyle->SetLabelSize( _textSize, "y" );
+//    _fittinoStyle->SetTitleSize( _textSize, "y" );
+//    _fittinoStyle->SetLabelSize( _textSize, "z" );
+//    _fittinoStyle->SetTitleSize( _textSize, "z" );
+    
+    _fittinoStyle->SetTitleOffset( 1.3, "x" );
+    _fittinoStyle->SetTitleOffset( 1.3, "y" );
+    _fittinoStyle->SetTitleOffset( 1.3, "z" );
+    
+    _fittinoStyle->SetFrameBorderMode( 0 );
+    
+    gROOT->SetStyle( "FITTINO" );
+    gROOT->ForceStyle();
+    
     
 }
 
