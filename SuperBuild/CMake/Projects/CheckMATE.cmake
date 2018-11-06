5c53f75d1a1a52240263af53d7a1f25a2da1ade1
Björn Sarrazin <sarrazin@physik.rwth-aachen.de>
2018-11-06 10:39:57 +0100

    Make sure CheckMATE uses the freshly installed Python version

--- SuperBuild/CMake/Projects/CheckMATE.cmake
+++ SuperBuild/CMake/Projects/CheckMATE.cmake
@@ -8,9 +8,9 @@ externalproject_add(
     ${CheckMATE}
     URL https://checkmate.hepforge.org/downloads/CheckMATE-2.0.26.tar.gz
     URL_MD5 d4af00cbf28948c4b1820abfbf6ef9d3
-    DEPENDS ${Delphes} ${HepMC2} ${Pythia8} ${MadGraph5}
+    DEPENDS ${Delphes} ${HepMC2} ${Pythia8} ${MadGraph5} ${CondaPackages}
     BUILD_IN_SOURCE ON
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} -E env CXX=${CMAKE_CXX_COMPILER} ./configure --with-rootsys=${ROOTSYS} --with-delphes=${Delphes_SOURCE_DIR} --with-hepmc=${HepMC2_ROOT_DIR} --with-pythia=${Pythia8_ROOT_DIR} --with-gziplib=${zlib_library_dir} --with-gzipinc=${ZLIB_INCLUDE_DIRS} --with-madgraph=${MadGraph5_ROOT_DIR}
+    CONFIGURE_COMMAND ${CMAKE_COMMAND} -E env CXX=${CMAKE_CXX_COMPILER} ./configure --with-rootsys=${ROOTSYS} --with-delphes=${Delphes_SOURCE_DIR} --with-hepmc=${HepMC2_ROOT_DIR} --with-pythia=${Pythia8_ROOT_DIR} --with-gziplib=${zlib_library_dir} --with-gzipinc=${ZLIB_INCLUDE_DIRS} --with-madgraph=${MadGraph5_ROOT_DIR} --with-python=${PYTHON_EXECUTABLE}
     BUILD_COMMAND ${CMAKE_COMMAND} -E env $(MAKE)
     INSTALL_COMMAND ${CMAKE_COMMAND} -E env $(MAKE) install
 
