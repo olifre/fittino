a8bc30a936fd3424f7da7bc0ea2f728fa5061284
Björn Sarrazin <sarrazin@physik.rwth-aachen.de>
2018-11-10 19:53:24 +0100

    Bugfix in patch of CheckMATE

--- SuperBuild/CMake/Scripts/patch_CheckMATE.cmake
+++ SuperBuild/CMake/Scripts/patch_CheckMATE.cmake
@@ -1,11 +1,20 @@
-FILE( READ tools/python/events.py fileContent )
+set( fileName tools/python/events.py )
+
+FILE( READ ${fileName} fileContent )
 
 # allow xsect to be zero
 set( expr "resultCollector.signal_err_sys = resultCollector.signal_normevents*xsecterr/xsect" )
-STRING( REPLACE ${expr} "${expr} if xsect != 0.0 else 0.0" fileContent ${fileContent} )
+STRING( REPLACE "${expr}" "${expr} if xsect != 0.0 else 0.0" fileContent ${fileContent} )
+
+FILE ( WRITE ${fileName} ${fileContent} )
+
+
+set( fileName tools/python/cls.py )
+
+FILE( READ ${fileName} fileContent )
 
 # allow scipy versions >= 1.0.0
 set ( expr "int(scipy.__version__.split('.')[1]) <= 10" )
-string( REPLACE ${expr} "${expr} AND int(scipy.__version__.split('.')[0]) == 0" fileContent ${fileContent} )
+string( REPLACE "${expr}" "${expr} AND int(scipy.__version__.split('.')[0]) == 0" fileContent ${fileContent} )
 
-FILE ( WRITE tools/python/events.py ${fileContent} )
+FILE ( WRITE ${fileName} ${fileContent} )
