412492fcdcd50b82538d95133957aa4f7e71f291
Björn Sarrazin <sarrazin@physik.rwth-aachen.de>
2018-11-10 17:03:48 +0100

    Allow CheckMATE to work with spicy versions >= 1.1.0

--- SuperBuild/CMake/Scripts/patch_CheckMATE.cmake
+++ SuperBuild/CMake/Scripts/patch_CheckMATE.cmake
@@ -1,4 +1,11 @@
 FILE( READ tools/python/events.py fileContent )
+
+# allow xsect to be zero
 set( expr "resultCollector.signal_err_sys = resultCollector.signal_normevents*xsecterr/xsect" )
 STRING( REPLACE ${expr} "${expr} if xsect != 0.0 else 0.0" fileContent ${fileContent} )
+
+# allow scipy versions >= 1.0.0
+set ( expr "int(scipy.__version__.split('.')[1]) <= 10" )
+string( REPLACE ${expr} "${expr} AND int(scipy.__version__.split('.')[0]) == 0" fileContent ${fileContent} )
+
 FILE ( WRITE tools/python/events.py ${fileContent} )
