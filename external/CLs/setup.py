a2f629ff633b26bc6433094b1c907194e087cf1c
Björn Sarrazin <sarrazin@physik.rwth-aachen.de>
2018-11-24 19:52:53 +0100

    Make CLs script a proper python package

---
+++ external/CLs/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="cls",
+    packages=setuptools.find_packages(),
+#    package_data={
+#        "cls.data": [
+#            "orthogonal.txt",
+#        ],
+#   }
+)
+
