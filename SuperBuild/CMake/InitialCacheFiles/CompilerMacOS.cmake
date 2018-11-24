6bdef4ce6da31bb1683a1d58c3a7a8a99d60af3d
Björn Sarrazin <sarrazin@physik.rwth-aachen.de>
2018-11-24 20:00:53 +0100

    Add root-config to macOS initialization script

--- SuperBuild/CMake/InitialCacheFiles/CompilerMacOS.cmake
+++ SuperBuild/CMake/InitialCacheFiles/CompilerMacOS.cmake
@@ -1,4 +1,5 @@
 set( CMAKE_CXX_COMPILER     /usr/bin/clang++              CACHE FILEPATH "" FORCE )
 set( CMAKE_C_COMPILER       /usr/bin/clang                CACHE FILEPATH "" FORCE )
+set( ROOT_CONFIG_EXECUTABLE /opt/local/bin/root-config6   CACHE FILEPATH "" FORCE )
 set( CMAKE_Fortran_COMPILER /opt/local/bin/gfortran-mp-7  CACHE FILEPATH "" FORCE )
 set( CMAKE_CXX_STANDARD 17  CACHE STRING "" FORCE )
