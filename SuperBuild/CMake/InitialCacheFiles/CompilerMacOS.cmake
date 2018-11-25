4978532668ecf5d0767c449592566867e5080555
Björn Sarrazin <sarrazin@physik.rwth-aachen.de>
2018-11-25 16:04:05 +0100

    Add Python 2.7 paths to macOS initialization script

--- SuperBuild/CMake/InitialCacheFiles/CompilerMacOS.cmake
+++
@@ -1,5 +0,0 @@
-set( CMAKE_CXX_COMPILER     /usr/bin/clang++              CACHE FILEPATH "" FORCE )
-set( CMAKE_C_COMPILER       /usr/bin/clang                CACHE FILEPATH "" FORCE )
-set( ROOT_CONFIG_EXECUTABLE /opt/local/bin/root-config6   CACHE FILEPATH "" FORCE )
-set( CMAKE_Fortran_COMPILER /opt/local/bin/gfortran-mp-7  CACHE FILEPATH "" FORCE )
-set( CMAKE_CXX_STANDARD 17  CACHE STRING "" FORCE )
---
+++ SuperBuild/CMake/InitialCacheFiles/MacOS.cmake
@@ -0,0 +1,10 @@
+set( CMAKE_CXX_COMPILER     /usr/bin/clang++              CACHE FILEPATH "" FORCE )
+set( CMAKE_C_COMPILER       /usr/bin/clang                CACHE FILEPATH "" FORCE )
+set( ROOT_CONFIG_EXECUTABLE /opt/local/bin/root-config6   CACHE FILEPATH "" FORCE )
+set( CMAKE_Fortran_COMPILER /opt/local/bin/gfortran-mp-7  CACHE FILEPATH "" FORCE )
+set( CMAKE_CXX_STANDARD 17  CACHE STRING "" FORCE )
+
+# For Python 2.7
+set( PYTHON_EXECUTABLE /opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin/python2.7 CACHE FILEPATH "" FORCE )
+set( PYTHON_LIBRARY /opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib CACHE FILEPATH "" FORCE )
+set( PYTHON_INCLUDE_DIR /opt/local/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7 CACHE PATH "" FORCE )
