5d165aff46ef7e514870a59d7a41c95804a7dc81
Björn Sarrazin <sarrazin@physik.rwth-aachen.de>
2018-12-09 20:11:35 +0100

    Downgrade CheckMATE to Version 2.0.11.

--- SuperBuild/CMake/Projects/CheckMATE.cmake
+++ SuperBuild/CMake/Projects/CheckMATE.cmake
@@ -1,13 +1,15 @@
 # see https://arxiv.org/abs/1611.09856, p.41 (Appendix A)
 # (As of version 2.0.26 README does not mention all options )
 
-set( CheckMATE CheckMATE-2.0.26 )
+set( CheckMATE CheckMATE-2.0.11 )
 
 externalproject_add(
 
     ${CheckMATE}
-    URL https://checkmate.hepforge.org/downloads/CheckMATE-2.0.26.tar.gz
-    URL_MD5 d4af00cbf28948c4b1820abfbf6ef9d3
+    #URL https://checkmate.hepforge.org/downloads/CheckMATE-2.0.26.tar.gz
+    #URL_MD5 d4af00cbf28948c4b1820abfbf6ef9d3
+    URL https://checkmate.hepforge.org/downloads/CheckMATE-2.0.11.tar.gz
+    URL_MD5 2ba417d2483e86c4334df18c9c44b74a
     DEPENDS ${Delphes} ${HepMC2} ${Pythia8} ${MadGraph5} ${CondaPackages}
     PATCH_COMMAND ${CMAKE_COMMAND} -P ${PROJECT_SOURCE_DIR}/CMake/Scripts/patch_CheckMATE.cmake
     CONFIGURE_COMMAND ${CMAKE_COMMAND} -E env CXX=${CMAKE_CXX_COMPILER} ./configure --with-rootsys=${ROOTSYS} --with-delphes=${Delphes_SOURCE_DIR} --with-hepmc=${HepMC2_ROOT_DIR} --with-pythia=${Pythia8_ROOT_DIR} --with-gziplib=${zlib_library_dir} --with-gzipinc=${ZLIB_INCLUDE_DIRS} --with-madgraph=${MadGraph5_ROOT_DIR} --with-python=${PYTHON_EXECUTABLE}
@@ -26,6 +28,7 @@ list( APPEND CACHE_ARGS "-DCheckMATE_EXECUTABLE:FILEPATH=${CheckMATE_ROOT_DIR}/b
 list( APPEND CACHE_ARGS "-DCheckMATE_TESTFILE:FILEPATH=${CheckMATE_ROOT_DIR}/bin/example_run_cards/auxiliary/testfile.hep" )
 
 list( APPEND PATH ${CheckMATE_ROOT_DIR}/bin )
+list( APPEND PYTHONPATH ${CheckMATE_ROOT_DIR}/tools/python )
 
 list( APPEND Fittino_DEPENDENCIES ${CheckMATE} )
 
