41ff81071a096d9afc77bc5def51d8640162e3bd
Björn Sarrazin <sarrazin@physik.rwth-aachen.de>
2018-12-15 17:31:45 +0100

    Include unvalidated 13 TeV analyses

--- SuperBuild/CMake/Scripts/patch_CheckMATE.cmake
+++ SuperBuild/CMake/Scripts/patch_CheckMATE.cmake
@@ -1,20 +1,25 @@
-set( fileName tools/python/events.py )
-
-FILE( READ ${fileName} fileContent )
-
 # allow xsect to be zero
+set( fileName tools/python/events.py )
+FILE( READ ${fileName} fileContent )
 set( expr "resultCollector.signal_err_sys = resultCollector.signal_normevents*xsecterr/xsect" )
 STRING( REPLACE "${expr}" "${expr} if xsect != 0.0 else 0.0" fileContent ${fileContent} )
-
 FILE ( WRITE ${fileName} ${fileContent} )
 
-
-set( fileName tools/python/cls.py )
-
-FILE( READ ${fileName} fileContent )
-
 # allow scipy versions >= 1.0.0
+set( fileName tools/python/cls.py )
+FILE( READ ${fileName} fileContent )
 set ( expr "int(scipy.__version__.split('.')[1]) <= 10" )
 string( REPLACE "${expr}" "${expr} and int(scipy.__version__.split('.')[0]) == 0" fileContent ${fileContent} )
+FILE ( WRITE ${fileName} ${fileContent} )
 
+# include unvalidated 13TeV CMS analyses
+set( fileName data/analysis_info/CMS_13TeV_analyses.txt )
+FILE( READ ${fileName} fileContent )
+string( REPLACE "#cms_" "cms_" fileContent ${fileContent} )
+FILE ( WRITE ${fileName} ${fileContent} )
+
+# include unvalidated 13TeV ATLAS analyses
+set( fileName data/analysis_info/ATLAS_13TeV_analyses.txt )
+FILE( READ ${fileName} fileContent )
+string( REPLACE "#atlas_" "atlas_" fileContent ${fileContent} )
 FILE ( WRITE ${fileName} ${fileContent} )
