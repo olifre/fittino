0deb37deb68925690d71f389399241f04b27bcda
Björn Sarrazin <sarrazin@physik.rwth-aachen.de>
2018-12-28 10:16:19 +0100

    In MacOS.cmake set CMAKE_Generator to Xcode for Fittino

--- SuperBuild/CMake/InitialCacheFiles/MacOS.cmake
+++ SuperBuild/CMake/InitialCacheFiles/MacOS.cmake
@@ -1,8 +1,9 @@
 set( CMAKE_CXX_COMPILER     /usr/bin/clang++              CACHE FILEPATH "" FORCE )
 set( CMAKE_C_COMPILER       /usr/bin/clang                CACHE FILEPATH "" FORCE )
+set( Fittino_CMAKE_GENERATOR Xcode  CACHE STRING "" FORCE )
 set( ROOT_CONFIG_EXECUTABLE /opt/local/bin/root-config6   CACHE FILEPATH "" FORCE )
-set( CMAKE_Fortran_COMPILER /opt/local/bin/gfortran-mp-7  CACHE FILEPATH "" FORCE )
 set( CMAKE_CXX_STANDARD 17  CACHE STRING "" FORCE )
+set( CMAKE_Fortran_COMPILER /opt/local/bin/gfortran-mp-7  CACHE FILEPATH "" FORCE )
 
 # For Python 2.7
 set( PYTHON_EXECUTABLE /opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin/python2.7 CACHE FILEPATH "" FORCE )
